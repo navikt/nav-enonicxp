14df876c9bd9076dca6db3bba7386a363dd64377
Bellfalasch <bellfalasch@gmail.com>
2017-12-21 13:56:38 +0100

    Some initial data-based output for newslist

--- src/main/resources/site/parts/articles-list-pressnews/articles-list-pressnews.js
+++ src/main/resources/site/parts/articles-list-pressnews/articles-list-pressnews.js
@@ -1,37 +1,52 @@
-var thymeleafLib = require('/lib/xp/thymeleaf');
+var libs = {
+	thymeleaf: require('/lib/xp/thymeleaf'),
+	portal: require('/lib/xp/portal'),
+	content: require('/lib/xp/content')
+};
+
 var view = resolve('articles-list-pressnews.html');
 
-function handleGet(req) {
+exports.get = function(req) {
+
+	/*
+	 * The following DataSources were used in the original CMS portlet:
+
+	<datasources>
+	  <datasource name="getContentBySection">
+	    <parameter name="menuItemKeys">${param.id}</parameter>
+	    <parameter name="levels">1</parameter>
+	    <parameter name="query">contenttype = 'nav.sidebeskrivelse' or contenttype = 'nav.pressemelding' or contenttype = 'nav.nyhet'</parameter>
+	    <parameter name="orderBy"/>
+	    <parameter name="index">${select(param.offset, 0)}</parameter>
+	    <parameter name="count">400</parameter>
+	    <parameter name="includeData">true</parameter>
+	    <parameter name="childrenLevel">1</parameter>
+	    <parameter name="parentLevel">0</parameter>
+	  </datasource>
+	</datasources>
+
+	*/
+	var results = libs.content.query({
+		start: 0,
+		count: 400,
+		sort: "",
+		query: "",
+		contentTypes: [
+			app.name + ":nav.sidebeskrivelse",
+			app.name + ":nav.pressemelding",
+			app.name + ":nav.nyhet"
+		]
+	});
 
     var params = {
-        partName: "articles-list-pressnews"
+        partName: "articles-list-pressnews",
+		contents: results.hits || null
     };
 
-    var body = thymeleafLib.render(view, params);
+    var body = libs.thymeleaf.render(view, params);
 
     return {
         contentType: 'text/html',
         body: body
     };
-}
-
-exports.get = handleGet;
-
-/*
- * The following DataSources were used in the original CMS portlet:
-
-<datasources>
-  <datasource name="getContentBySection">
-    <parameter name="menuItemKeys">${param.id}</parameter>
-    <parameter name="levels">1</parameter>
-    <parameter name="query">contenttype = 'nav.sidebeskrivelse' or contenttype = 'nav.pressemelding' or contenttype = 'nav.nyhet'</parameter>
-    <parameter name="orderBy"/>
-    <parameter name="index">${select(param.offset, 0)}</parameter>
-    <parameter name="count">400</parameter>
-    <parameter name="includeData">true</parameter>
-    <parameter name="childrenLevel">1</parameter>
-    <parameter name="parentLevel">0</parameter>
-  </datasource>
-</datasources>
-
-*/
+};
