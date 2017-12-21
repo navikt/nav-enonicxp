63c8754bd66803aebdbb157715b795fe7201cdfd
Bellfalasch <bellfalasch@gmail.com>
2017-12-21 13:18:04 +0100

    Initial work on pressemelding part

--- src/main/resources/site/parts/articles-list-pressnews/articles-list-pressnews.js
+++ src/main/resources/site/parts/articles-list-pressnews/articles-list-pressnews.js
@@ -21,16 +21,6 @@ exports.get = handleGet;
  * The following DataSources were used in the original CMS portlet:
 
 <datasources>
-  <!--datasource name="getContent">
-    <parameter name="contentKeys">${select(param.key,0)}</parameter>
-    <parameter name="query"/>
-    <parameter name="orderBy"/>
-    <parameter name="index">0</parameter>
-    <parameter name="count">1</parameter>
-    <parameter name="includeData">true</parameter>
-    <parameter name="childrenLevel">0</parameter>
-    <parameter name="parentLevel">0</parameter>
-  </datasource-->
   <datasource name="getContentBySection">
     <parameter name="menuItemKeys">${param.id}</parameter>
     <parameter name="levels">1</parameter>
