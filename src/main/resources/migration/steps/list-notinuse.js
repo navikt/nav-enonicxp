6da0632dc9ba547224f1135ef58e9b6ed5025750
Bellfalasch <bellfalasch@gmail.com>
2018-01-04 15:13:08 +0100

    Tune to progress status

--- src/main/resources/migration/steps/list-notinuse.js
+++ src/main/resources/migration/steps/list-notinuse.js
@@ -15,24 +15,24 @@ exports.execute = function () {
 		start: 0,
 		count: 10000,
 		sort: "",
-		query: "_path LIKE '/content/*'",
+		query: "_path LIKE '/content/sites/www.nav.no/*'",
 	});
 
     log.info(result.total + ' contents to list.');
 
     for (var i = 0; i < result.hits.length; i++) {
         libs.task.progress({
-            info: 'Content ' + result.hits._name,
+            info: 'Content ' + result.hits[i]._name,
             current: i + 1,
             total: result.total
         });
-
     }
 
     log.info(result.total + ' contents found.');
     log.info('---------------------------------');
 
-    libs.task.progress({ info: result.total + ' contents found.'});
+    //libs.task.progress({ info: result.total + ' contents found.'});
+    libs.task.progress({ info: templateList });
 
     return {
         ok: true
