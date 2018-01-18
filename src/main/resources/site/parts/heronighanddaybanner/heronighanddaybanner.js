43c0241546985ee004ed38c1ab2f34e1a1448f64
Bellfalasch <bellfalasch@gmail.com>
2018-01-18 15:30:05 +0100

    Add lib-util

--- src/main/resources/site/parts/heronighanddaybanner/heronighanddaybanner.js
+++ src/main/resources/site/parts/heronighanddaybanner/heronighanddaybanner.js
@@ -2,6 +2,9 @@ var portalLib = require('/lib/xp/portal');
 var contentLib = require('/lib/xp/content');
 var thymeleafLib = require('/lib/xp/thymeleaf');
 var utils = require('/lib/nav-utils');
+var libs = {
+	util: require('/lib/enonic/util')
+}
 
 var view = resolve('heronighanddaybanner.html');
 
