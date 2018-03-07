f6476a7762493e6e080fa78cc7823a0bdc9a5fb1
Alex Rodriguez <aro@enonic.com>
2018-03-07 13:34:28 +0100

    Script - Script to automate moving a list of specified content (#7)

--- src/main/resources/assets/migration/js/main.js
+++ src/main/resources/assets/migration/js/main.js
@@ -3,6 +3,7 @@ const serviceUrl = document.currentScript.getAttribute('data-serviceurl');
 var deleteTaskId, listTaskId;
 $(function () {
     $('#deleteContent').on('click', executeDeleteContent);
+    $('#moveContent').on('click', executeMoveContent);
     $('#listNotinuse').on('click', executeListNotinuse);
 });
 
@@ -34,6 +35,36 @@ function deleteContentDone(result) {
     $('#deleteContent').show();
     $('#deleteContentExecuting').hide();
     $('#deleteContentResult').show().text(result);
+}
+
+function executeMoveContent(e) {
+    e.preventDefault();
+    $('#moveContent,#moveContentResult').hide();
+    $('#moveContentExecuting').show();
+
+    $.ajax({
+        type: "POST",
+        url: serviceUrl,
+        dataType: 'json',
+        data: {
+            action: 'moveContent'
+        },
+        success: function (data) {
+            moveTaskId = data.id;
+            checkStatus(moveTaskId, moveContentDone);
+        },
+        error: function () {
+            console.log(arguments);
+            $('#moveContent').show();
+            $('#moveContentExecuting').hide();
+        }
+    });
+}
+
+function moveContentDone(result) {
+    $('#moveContent').show();
+    $('#moveContentExecuting').hide();
+    $('#moveContentResult').show().text(result);
 }
 
 function executeListNotinuse(e) {
