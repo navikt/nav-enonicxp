0f63bb641870eed400043a782f90d0ae70fbaf78
Per Arne Drevland <per.arne.drevland@item.no>
2018-07-26 13:19:29 +0200

    IV-144 Fjernet log.info-kall og unødvendige kommentarer

--- src/main/resources/site/pages/page-nav/page-nav.js
+++ src/main/resources/site/pages/page-nav/page-nav.js
@@ -41,7 +41,7 @@ function handleGet(req) {
     var bodyClassExtras = "contentpage";
 
 	var social = getSocial(content, req);
-	log.info(JSON.stringify(social));
+
     var model = {
 		isEditMode: (req.mode === 'edit'),
         context: req,
@@ -106,31 +106,4 @@ function setOg(property, content, name) {
 	return '<meta ' + name +'="' + og + ':' + property + '" content="' + content + '" />';
 }
 
-/*
- * The following DataSources were used in the original CMS page template:
 
- <datasources>
-  <datasource name="getMenuBranch">
-    <parameter name="menuItemKey">${portal.pageKey}</parameter>
-    <parameter name="includeTopLevel">false</parameter>
-    <paramater name="startLevel">1</paramater>
-    <parameter name="levels">5</parameter>
-  </datasource>
-  <datasource name="getContent">
-    <parameter name="contentKeys">${select(param.key,-1)}</parameter>
-    <parameter name="query"/>
-    <parameter name="orderBy"/>
-    <parameter name="index">0</parameter>
-    <parameter name="count">1</parameter>
-    <parameter name="includeData">true</parameter>
-    <parameter name="childrenLevel">1</parameter>
-    <parameter name="parentLevel">0</parameter>
-  </datasource>
-  <datasource name="getMenu" result-element="languages">
-    <parameter name="siteKey">20</parameter>
-    <parameter name="tagItem">${portal.pageKey}</parameter>
-    <parameter name="levels">1</parameter>
-  </datasource>
-</datasources>
-
- */
