54155346992c664805440fedb334303431477dce
PerOlavM <per.olav.mariussen@nav.no>
2018-08-01 11:03:04 +0200

    Håndtering av metadata

--- src/main/resources/site/pages/page-nav/page-nav.js
+++ src/main/resources/site/pages/page-nav/page-nav.js
@@ -6,11 +6,36 @@ var libs = {
 	util: require('/lib/enonic/util')
 };
 var view = resolve('page-nav.html');
-var accessibleLetters = 'abcdefghijklmnopqrstuvwxyzæøå'.split('');
 
 function handleGet(req) {
     var site = libs.portal.getSite();
     var content = libs.portal.getContent();
+
+    var imageUrl = getImageUrl(content.data.text);
+    imageUrl = (imageUrl ? imageUrl : libs.portal.assetUrl({path: '/img/navno/social-share-fallback.png'}));
+    var title = content.displayName + " - www.nav.no";
+    var description = (content.data.ingress ? content.data.ingress :
+		"NAV forvalter en tredjedel av statsbudsjettet gjennom ordninger som dagpenger, arbeidsavklaringspenger, " +
+		"sykepenger, pensjon, barnetrygd og kontantstøtte."
+    );
+    var metadata = {
+        "description": 			description,
+		"og:sitename": 			"NAV",
+        "og:type": 				"article",
+        "og:title": 			title,
+        "og:url": 				req.url.split('?')[0],
+		"og:image":				imageUrl,
+		"og:description": 		description,
+		"twitter:card":			"summary_large_image",
+        "twitter:domain": 		"nav.no",
+        "twitter:title": 		title,
+		"twitter:image:src":	imageUrl,
+        "twitter:description": 	description
+	};
+    var metaTags = [];
+    forIn(metadata, function(val, key){
+    	metaTags.push(setMetaTag(key,val));
+	});
 
     var menuItems = libs.menu.getSubMenus(site, 4);
     menuItems = menuItems[0];
@@ -52,8 +77,6 @@ function handleGet(req) {
 	// TODO: Avklare behovet for en egen contentpage class, nå settes dette på alle sider
     var bodyClassExtras = "contentpage";
 
-	var social = getSocial(content, req);
-
     var model = {
 		isEditMode: (req.mode === 'edit'),
         context: req,
@@ -65,7 +88,7 @@ function handleGet(req) {
         frontPageUrl: libs.portal.pageUrl({id: site._id}),
         contentAZPage: libs.portal.serviceUrl({service: 'contentAZ'}),
         seksjonsSider: seksjonssider,
-        accessibleLetters: accessibleLetters,
+        accessibleLetters: 'abcdefghijklmnopqrstuvwxyzæøå'.split(''),
         menu: menuItems,
         breadcrumbs: breadcrumbs,
         bodyClassExtras: bodyClassExtras
@@ -75,48 +98,31 @@ function handleGet(req) {
         contentType: 'text/html',
         body: libs.thymeleaf.render(view, model),
 		pageContributions: {
-        	headBegin: social ? social : []
+        	headBegin: metaTags
 		}
     };
 }
 
 exports.get = handleGet;
 
-function getSocial(content, req) {
-	if (!content.data.social) return null;
-	var social = Array.isArray(content.data.social) ? content.data.social : [content.data.social];
-	return social.reduce(function (total, value) {
-		if (value === 'facebook') {
-
-			total.push(setOg('url', req.url.split('?')[0]));
-			total.push(setOg('type', 'article'));
-			total.push(setOg('title', content.displayName));
-			total.push(setOg('description', content.data.ingress));
-			total.push(setOg('image', libs.portal.assetUrl({path: 'beta.nav.no/images/social-share-fallback.png'})));
-		}
-		else if (value === 'twitter') {
-			total.push(setOg('card', 'summary_large_image', 'name'));
-			total.push(setOg('title', content.displayName, 'name'));
-			total.push(setOg('description', content.data.ingress, 'name'));
-			total.push(setOg('domain', 'nav.no', 'name'));
-			total.push(setOg('image:src', libs.portal.assetUrl({path: 'beta.nav.no/images/social-share-fallback.png'}), 'name'));
-		}
-		else if (value === 'linkedin' && total.indexOf(setOg('type', 'article')) === -1) {
-            total.push(setOg('url', req.url.split('?')[0]));
-            total.push(setOg('type', 'article'));
-            total.push(setOg('title', content.displayName));
-            total.push(setOg('description', content.data.ingress));
-            total.push(setOg('image', libs.portal.assetUrl({path: 'beta.nav.no/images/social-share-fallback.png'})));
-		}
-		return total
-	},[])
-
+function setMetaTag(property, content) {
+	var name = (property.indexOf("og:")>-1 ? "property" : "name");
+	return "<meta " + name + "='" + property + "' content='" + content + "' />";
 }
 
-function setOg(property, content, name) {
-	var og = name ? 'twitter' : 'og';
-	name = name || 'property';
-	return '<meta ' + name +'="' + og + ':' + property + '" content="' + content + '" />';
+function forIn(obj, fn, thisObj) {
+    var key;
+    for (key in obj) {
+        if (exec(fn, obj, key, thisObj) === false) {
+            break;
+        }
+    }
+    function exec(fn, obj, key, thisObj){
+        return fn.call(thisObj, obj[key], key, obj);
+    }
+    return forIn;
 }
 
-
+function getImageUrl(text) {
+	return null; //TODO: Hva skal være regel for deling av bilder?
+}
