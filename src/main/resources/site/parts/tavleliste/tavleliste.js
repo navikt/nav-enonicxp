9558a114243841bec418ab5661085a81215ea615
Per Arne Drevland <per.arne.drevland@item.no>
2018-08-23 09:51:04 +0200

    IV-103 Refakorering: endret content.published.from til from

--- src/main/resources/site/parts/tavleliste/tavleliste.js
+++ src/main/resources/site/parts/tavleliste/tavleliste.js
@@ -18,7 +18,7 @@ exports.get = function(req) {
     // Define the model
     var model = {
         published: utils.dateTimePublished(cont, cont.language || 'no'),
-        content: cont,
+        from: cont.published.from,
         heading: cont.data.heading || cont.displayName,
         ingress: cont.data.ingress,
         items: items
