9b0e395b57f6c12bf6e446f10a51a533879b7e0a
EivindSkifjeld <eivind.skifjeld@nav.no>
2018-09-10 10:55:31 +0200

    Endringer i main-article innholdstype ihht. IV-260 Visning av displayname

--- src/main/resources/site/parts/main-article-related-content/main-article-related-content.js
+++ src/main/resources/site/parts/main-article-related-content/main-article-related-content.js
@@ -1,8 +1,6 @@
 var thymeleafLib = require('/lib/xp/thymeleaf');
 var portal = require('/lib/xp/portal');
 var contentLib = require('/lib/xp/content');
-var trans = require('/lib/contentTranslator');
-var utils = require('/lib/nav-utils');
 var view = resolve('main-article-related-content.html');
 
 function handleGet(req) {
@@ -24,10 +22,7 @@ function handleGet(req) {
         });
     });
 
-    trans.logBeautify(links);
-
     var hasMenuLists = (links.length > 0);
-    log.info(hasMenuLists);
     var params = {
         relatedContentList: links,
         hasMenuList: hasMenuLists
@@ -42,46 +37,7 @@ function handleGet(req) {
 }
 
 
-
 exports.get = handleGet;
 function forceArr(element) {
     return Array.isArray(element) ? element : [element]
 }
-/*
- * The following DataSources were used in the original CMS portlet:
-
-<datasources>
-    <datasource condition="${isnotempty(param.submenu)}" name="getContent">
-      <parameter name="contentKeys">${replace(param.kap, '[a-zA-Z]+', '0')}</parameter>
-      <parameter name="query"/>
-      <parameter name="orderBy"/>
-      <parameter name="index">0</parameter>
-      <parameter name="count">1</parameter>
-      <parameter name="includeData">true</parameter>
-      <parameter name="childrenLevel">1</parameter>
-      <parameter name="parentLevel">0</parameter>
-    </datasource>
-    <datasource condition="${isnotempty(param.kap)}" name="getRelatedContent" result-element="relatedforms">
-      <parameter name="contentKeys">${replace(param.kap, '[a-zA-Z]+', '0')}</parameter>
-      <parameter name="relation">1</parameter>
-      <parameter name="query"/>
-      <parameter name="orderBy"/>
-      <parameter name="index">0</parameter>
-      <parameter name="count">100</parameter>
-      <parameter name="includeData">true</parameter>
-      <parameter name="childrenLevel">1</parameter>
-      <parameter name="parentLevel">0</parameter>
-    </datasource>
-    <datasource name="getContent" result-element="rapporthandbok">
-      <parameter name="contentKeys">${select(param.key, -1)}</parameter>
-      <parameter name="query">contenttype = 'Rapport_handbok'</parameter>
-      <parameter name="orderBy"/>
-      <parameter name="index">0</parameter>
-      <parameter name="count">1</parameter>
-      <parameter name="includeData">true</parameter>
-      <parameter name="childrenLevel">1</parameter>
-      <parameter name="parentLevel">0</parameter>
-    </datasource>
-  </datasources>
-
-*/
