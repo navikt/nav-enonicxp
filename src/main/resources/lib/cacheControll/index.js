a91d833caed401824abf72365ecf4c8fc128d498
PerOlavM <per.olav.mariussen@nav.no>
2019-03-28 11:09:49 +0100

    Nytt navn på default repo i 7.0

--- src/main/resources/lib/cacheControll/index.js
+++ src/main/resources/lib/cacheControll/index.js
@@ -7,7 +7,7 @@ var standardCache = {
 var etag = Date.now().toString(16);
 var nodeLib = require('/lib/xp/node');
 var repo = nodeLib.connect({
-    repoId: 'cms-repo',
+    repoId: 'com.enonic.cms.default',
     branch: 'master',
     principals: ['role:system.admin']
 });
