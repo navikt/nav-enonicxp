136ad8bdb53f6400a16a2979cafda9cc2fa1bb02
Øyvind Nordli <oe.nordli@gmail.com>
2019-04-10 17:11:01 +0200

    add search result template and migrate search priority

--- src/main/resources/lib/priorityElements/index.js
+++ src/main/resources/lib/priorityElements/index.js
@@ -1,92 +1,99 @@
 var content = require('/lib/xp/content');
 var context = require('/lib/xp/context');
 var http = require('/lib/http-client');
-exports.handle = function (socket) {
+var tools = require('/lib/tools');
+
+exports.handle = function(socket) {
     var elements = createElements();
     socket.emit('newTask', elements);
-    socket.on('convert-priority', function () {
-        context.run({
-            repository: 'cms-repo',
-            branch: 'draft',
-            user: {
-                login: 'pad',
-                userStore: 'system'
+    socket.on('convert-priority', function() {
+        context.run(
+            {
+                repository: 'cms-repo',
+                branch: 'draft',
+                user: {
+                    login: 'pad',
+                    userStore: 'system'
+                },
+                principals: ['role:system.admin']
             },
-            principals: ["role:system.admin"]
-        }, function () {
-            convertPriority(socket);
-        })
-
+            function() {
+                convertPriority(socket);
+            }
+        );
     });
-
-
-}
+};
 
 function convertPriority(socket) {
     var req = http.request({
         url: 'http://localhost:8080/status/osgi.bundle',
         contentType: 'application/json'
     });
-    log.info(JSON.stringify(JSON.parse(req.body), null, 4));
-    var hasSearch = (req.status === 200 && JSON.parse(req.body).bundles.reduce(function (t, el) {
-      return t || el.name === 'no.nav.navno.search'
-    },false));
+    // log.info(JSON.stringify(JSON.parse(req.body), null, 4));
+    var hasSearch =
+        req.status === 200 &&
+        JSON.parse(req.body).bundles.reduce(function(t, el) {
+            return t || el.name === 'navno.nav.no.search';
+        }, false);
     if (!hasSearch) return socket.emit('convert-complete', 'Nav Search unavailable');
-    var priorities = content.get({ key: '/content/sok/nav-no/prioritert/generelle'});
-
-
-    var data = createData(priorities.data.forslag);
-
-    log.info(JSON.stringify(data, null, 4));
+    var priorities = content.get({ key: '/content/sok/nav-no/prioritert/generelle' });
 
     var xpPri = content.get({
         key: '/www.nav.no/prioriterte-elementer'
     });
-
-
-
     if (!xpPri) {
-        //var p = content.create({
-
-//        })
+        content.create({
+            displayName: 'prioriterte-elementer',
+            parentPath: '/www.nav.no/',
+            contentType: 'base:folder',
+            data: {}
+        });
     }
+    var xpPriExternal = content.get({
+        key: '/www.nav.no/prioriterte-elementer-eksternt'
+    });
+
+    if (!xpPriExternal) {
+        content.create({
+            displayName: 'prioriterte-elementer-eksternt',
+            parentPath: '/www.nav.no/',
+            contentType: 'base:folder',
+            data: {}
+        });
+    }
+
+    createData(priorities.data.forslag);
 }
 
 function createData(forslag) {
-    return forslag.map(function(el) {
-        return {
-            content: testPath(el),
-            keywords: el.keywords.split(' ')
+    forslag.forEach(function(el) {
+        var info = tools.getIdFromUrl(el.lenke);
+        if (info.external === false && info.invalid === false) {
+            content.create({
+                displayName: el.tittel,
+                parentPath: '/www.nav.no/prioriterte-elementer/',
+                contentType: 'navno.nav.no.search:search-priority',
+                data: {
+                    content: info.refId,
+                    keywords: el.keywords.split(' ')
+                }
+            });
+        } else {
+            var split = el.lenke.split('.');
+            var applicationName = [split[0], split[1], split[2] ? split[2].split('/')[0] : ''].join('.');
+            content.create({
+                displayName: el.tittel,
+                parentPath: '/www.nav.no/prioriterte-elementer-eksternt/',
+                contentType: 'navno.nav.no.search:search-api2',
+                data: {
+                    applicationName: applicationName,
+                    keywords: el.keywords.split(' '),
+                    url: el.lenke,
+                    ingress: el.ingress
+                }
+            });
         }
-    }).reduce(function (t, el) {
-        if (el.content) t.push(el)
-        return t;
-    }, []);
-}
-
-function testPath(element) {
-    if (element.dokumenttype === 'Skjema') return undefined;
-    var r = element.lenke.replace(/http[s]?:\//, '');
-    var isIntern = r.startsWith('/www.nav.no');
-    if (isIntern) {
-        var decoded = decodeURI(r).replace(/\+/g, '-').toLowerCase()
-            .replace(/ø/g, 'o')
-            .replace(/æ/g,'ae')
-            .replace(/å/g, 'a')
-            .replace(/%2c/, '')
-            .replace(/(--\d*)$/g, '')
-            .replace(/---/g, '-').replace(/.\d*.cms/, '');
-        var c = content.get({key: decoded});
-        if (c) return c._id;
-        else {
-            return undefined;
-        }
-    }
-    else {
-        log.info(r);
-        log.info(element.keywords);
-    }
-
+    });
 }
 
 function createElements() {
@@ -98,7 +105,6 @@ function createElements() {
                 {
                     tag: 'p',
                     text: 'Konverter prioriterte elementer'
-
                 },
                 {
                     tag: 'button',
@@ -112,5 +118,5 @@ function createElements() {
                 }
             ]
         }
-    }
+    };
 }
