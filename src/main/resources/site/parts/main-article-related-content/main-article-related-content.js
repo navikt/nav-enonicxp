a53b7edff1e34cf646b19223d566a4ca927ab07a
Øyvind Nordli <oe.nordli@gmail.com>
2019-05-07 16:17:52 +0200

    lint main-article-related-content

--- src/main/resources/site/parts/main-article-related-content/main-article-related-content.js
+++ src/main/resources/site/parts/main-article-related-content/main-article-related-content.js
@@ -10,43 +10,55 @@ function handleGet(req) {
         var content = portal.getContent();
         var selectNames = langLib.parseBundle(content.language).related_content.select;
         var menuListItems = content.data.menuListItems || {};
-        var keys = ["selfservice", "form-and-application",  "process-times", "related-information", "international", "report-changes", "rates",  "appeal-rights", "membership", "rules-and-regulations"];
-        var linkList = keys.map(function(el) {
-            if(!menuListItems[el]) return undefined;
-            var links = forceArr(menuListItems[el].link);
-            return {
-                name: selectNames[el] !== undefined ? selectNames[el] : '',
-                links: links
-                    .map(function(contentId) {
-                        var element = contentLib.get({ key: contentId });
-                        if (!element) return undefined;
-                        var link = "";
-                        if(element.type === 'media:document') {
-                            link = portal.attachmentUrl({ id: element._id, download: true });
-                        } else if(element.type === 'no.nav.navno:Ekstern_lenke'){
-                            var url = element.data.url;
-                            if(url.indexOf('http') !== 0) {
-                                url = 'https://' + url;
+        var keys = [
+            'selfservice',
+            'form-and-application',
+            'process-times',
+            'related-information',
+            'international',
+            'report-changes',
+            'rates',
+            'appeal-rights',
+            'membership',
+            'rules-and-regulations'
+        ];
+        var linkList = keys
+            .map(function(el) {
+                if (!menuListItems[el]) return undefined;
+                var links = forceArr(menuListItems[el].link);
+                return {
+                    name: selectNames[el] !== undefined ? selectNames[el] : '',
+                    links: links
+                        .map(function(contentId) {
+                            var element = contentLib.get({ key: contentId });
+                            if (!element) return undefined;
+                            var link = '';
+                            if (element.type === 'media:document') {
+                                link = portal.attachmentUrl({ id: element._id, download: true });
+                            } else if (element.type === 'no.nav.navno:Ekstern_lenke') {
+                                var url = element.data.url;
+                                if (url.indexOf('http') !== 0) {
+                                    url = 'https://' + url;
+                                }
+                                link = url;
+                            } else {
+                                link = portal.pageUrl({ id: contentId });
                             }
-                            link = url;
-                        } else {
-                            link = portal.pageUrl({ id: contentId });
-                        }
-                        return {
-                            title: element.displayName,
-                            link: link
-                        };
-                    }).reduce(function(t, el) {
-                        if (el) t.push(el);
-                        return t;
-                    }, [])
-            };
-
-        })
-        .reduce(function(t, el) {
-            if (el && el.links && el.links.length > 0) t.push(el);
-            return t;
-        }, []);
+                            return {
+                                title: element.displayName,
+                                link: link
+                            };
+                        })
+                        .reduce(function(t, el) {
+                            if (el) t.push(el);
+                            return t;
+                        }, [])
+                };
+            })
+            .reduce(function(t, el) {
+                if (el && el.links && el.links.length > 0) t.push(el);
+                return t;
+            }, []);
 
         var hasMenuLists = linkList.length > 0;
         var params = {
