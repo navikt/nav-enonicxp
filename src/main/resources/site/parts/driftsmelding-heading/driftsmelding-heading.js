09ffefb4f6f29b2df8aa1b1e860d813c9cd558e6
PerOlavM <per.olav.mariussen@nav.no>
2019-05-10 09:35:04 +0200

    Utvidet templates til å håndtere driftsmeldinger Lagt til visning av eventuell driftsmelding

--- src/main/resources/site/parts/driftsmelding-heading/driftsmelding-heading.js
+++ src/main/resources/site/parts/driftsmelding-heading/driftsmelding-heading.js
@@ -2,27 +2,52 @@ var libs = {
     thymeleaf: require('/lib/thymeleaf'),
     portal: require('/lib/xp/portal'),
     content: require('/lib/xp/content'),
-    lang: require('/lib/i18nUtil'),
-    log: require('/lib/contentTranslator')
+    context: require('/lib/xp/context'),
+    lang: require('/lib/i18nUtil')
 };
 var view = resolve('driftsmelding-heading.html');
 
 function handleGet(req) {
-    var content = libs.portal.getContent();
-    var language = content.language || 'no';
-    var linktext = libs.lang.parseBundle(language).driftsmelding.linktext;
-    //var message = libs.content.get({key:'/www.nav.no/no/driftsmelding'});
-    var model = {
-        showheading: true,
-        heading: 'Problemer i NAV',
-        linkur: 'https://www.nav.no/no/driftsmelding',
-        linktext: linktext
-    };
+    //Må kjøre i context av master-branch, ellers vil preview i Content studio alltid vise en driftsmelding
+    return libs.context.run(
+        {
+            repository: 'com.enonic.cms.default',
+            branch: 'master',
+            user: {
+                login: 'su',
+                userStore: 'system'
+            },
+            principals: ['role:system.admin']
+        },
+        function() {
+            //Henter ut eventuell publisert driftsmelding
+            var message = libs.content.getChildren({
+                key:'/www.nav.no/no/driftsmeldinger',
+                start: 0,
+                count: 1,
+                sort: '_modifiedTime DESC'
+            });
+            if ( message && message.hits.length > 0 ) {
+                var content = libs.portal.getContent();
+                var language = content.language || 'no';
+                //Henter ut visningsnavnet og urlen til meldingen, lenketeksten er fast
+                var model = {
+                    heading: message.hits[0].displayName,
+                    linkurl: libs.portal.pageUrl({type: "absolute", path: message.hits[0]._path}).replace("http:", "https:"),
+                    linktext: libs.lang.parseBundle(language).driftsmelding.linktext
+                };
 
-    return {
-        contentType: 'text/html',
-        body: libs.thymeleaf.render(view, model)
-    };
+                return {
+                    contentType: 'text/html',
+                    body: libs.thymeleaf.render(view, model)
+                };
+            }
+            else {
+                //Ingen publiserte driftsmeldinger
+                return null;
+            }
+        }
+    );
 }
 
 exports.get = handleGet;
