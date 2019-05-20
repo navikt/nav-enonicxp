a1ede5dcea0311eb83417e2a8c5df03d6b1ad64c
Øyvind Nordli <oe.nordli@gmail.com>
2019-05-20 16:00:10 +0200

    add babel and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    env: {
+        commonjs: true,
+        es6: true,
+        node: true
+    },
+    extends: 'standard',
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+        log: 'readonly',
+        app: 'readonly'
+    },
+    parserOptions: {
+        ecmaVersion: 2018
+    },
+    rules: {
+        semi: ['error', 'always'],
+        indent: ['error', 4],
+        curly: ['error', 'all'],
+        'no-extra-parens': ['off', 'all']
+    }
+};
