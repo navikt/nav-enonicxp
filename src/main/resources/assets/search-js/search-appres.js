ad4b3798361c60473f461d19a5cb98b509614642
Tom-Anders Røst <trost@Tom-Anderss-MacBook-Pro.local>
2020-03-06 12:24:08 +0100

    IV-870, fix for clientside aggregation numbers
    - Also added some fault checks

--- src/main/resources/assets/search-js/search-appres.js
+++ src/main/resources/assets/search-js/search-appres.js
@@ -1,5 +1,5 @@
-(function () {
-    function init () {
+(function() {
+    function init() {
         $('#searchbar').submit(submitForm);
         $('#fasettform').submit(submitForm);
         $('#sort input[name="s"]').on('change', changeSort);
@@ -15,14 +15,19 @@
         }
     }
     init();
-    function setC (n) { // count
+    function setC(n) {
+        // count
         $('input[name=c]').val(n);
     }
-    function update (e) {
+    function update(e) {
         e.preventDefault();
 
         var $th = $('#fasettform');
-        window.history.pushState(null, document.title, window.location.origin + window.location.pathname + '?' + $th.serialize());
+        window.history.pushState(
+            null,
+            document.title,
+            window.location.origin + window.location.pathname + '?' + $th.serialize()
+        );
 
         var count = Number($('input[name=c]').val());
         var $resultList = $('.sokeresultatliste');
@@ -30,31 +35,70 @@
             type: $th.attr('method'),
             url: $th.attr('action'),
             data: $th.serialize() + '&start=' + (count - 1),
-            success: function (data) {
+            success: function(data) {
                 // update heading
                 $('#search-result-heading').text(data.fasett);
                 // append all hits to result list
                 for (var i = 0; i < data.hits.length; i += 1) {
                     var hit = data.hits[i];
-                    var hitTemplate = '<li class="' + hit.className + '">' + '<a href="' + hit.href + '">' + '<h2>' + hit.displayName + '</h2><p>' + hit.displayPath + '</p>';
+                    var hitTemplate =
+                        '<li class="' +
+                        hit.className +
+                        '">' +
+                        '<a href="' +
+                        hit.href +
+                        '">' +
+                        '<h2>' +
+                        hit.displayName +
+                        '</h2><p>' +
+                        hit.displayPath +
+                        '</p>';
                     if (!hit.officeInformation) {
                         // normal
                         hitTemplate += '<p class="resultattekst">' + hit.highlight + '</p>';
                         if (hit.publishedString) {
-                            hitTemplate += '<p class="published-time">' + '<span>' + hit.publishedString + '</span>' + '</p>';
+                            hitTemplate +=
+                                '<p class="published-time">' +
+                                '<span>' +
+                                hit.publishedString +
+                                '</span>' +
+                                '</p>';
                         }
                     } else {
                         // officeInformation/enhetsinformasjon
                         hitTemplate += '<table>' + '<tbody>' + '<tr>';
-                        hitTemplate += '<td>' + 'TELEFON:' + '</td>' + '<td>' + hit.officeInformation.phone + '</td>';
-                        hitTemplate += '</tr>' + '<tr>' + '<td>' + 'PUBLIKUMSMOTTAK:' + '</td>' + '<td>' + '<ul>';
-                        for (var j = 0; j < hit.officeInformation.audienceReceptions.length; j += 1) {
-                            hitTemplate += '<li class="listeinnisoketreff">' + '<span>' + hit.officeInformation.audienceReceptions[j] + '</span>' + '</li>';
+                        hitTemplate +=
+                            '<td>' +
+                            'TELEFON:' +
+                            '</td>' +
+                            '<td>' +
+                            hit.officeInformation.phone +
+                            '</td>';
+                        hitTemplate +=
+                            '</tr>' +
+                            '<tr>' +
+                            '<td>' +
+                            'PUBLIKUMSMOTTAK:' +
+                            '</td>' +
+                            '<td>' +
+                            '<ul>';
+                        for (
+                            var j = 0;
+                            j < hit.officeInformation.audienceReceptions.length;
+                            j += 1
+                        ) {
+                            hitTemplate +=
+                                '<li class="listeinnisoketreff">' +
+                                '<span>' +
+                                hit.officeInformation.audienceReceptions[j] +
+                                '</span>' +
+                                '</li>';
                         }
                         hitTemplate += '</ul>' + '</td>' + '</tr>' + '</tbody>' + '</table>';
                     }
                     if (hit.priority) {
-                        hitTemplate += '<p data-th-if="$' + hit.priority + '">Innhold anbefalt av NAV</p>';
+                        hitTemplate +=
+                            '<p data-th-if="$' + hit.priority + '">Innhold anbefalt av NAV</p>';
                     }
                     hitTemplate += '</a>' + '</li>';
                     $resultList.append($(hitTemplate));
@@ -65,12 +109,40 @@
 
                 // update date periods
                 var totalInDateCount = 0;
-                for (var k = 0; k < data.aggregations.Tidsperiode.buckets.length; k += 1) {
-                    var datePeriod = data.aggregations.Tidsperiode.buckets[k];
+                var timeIntervals = data.aggregations.Tidsperiode.buckets.map(function(elem, ix) {
+                    elem.index = ix;
+                    return elem;
+                });
+
+                var order = [
+                    'Siste 7 dager',
+                    'Siste 30 dager',
+                    'Siste 12 måneder',
+                    'Eldre enn 12 måneder',
+                ];
+                timeIntervals = timeIntervals.sort(function(a, b) {
+                    if (order.indexOf(a.key) > order.indexOf(b.key)) {
+                        return 1;
+                    }
+                    if (order.indexOf(b.key) > order.indexOf(a.key)) {
+                        return -1;
+                    }
+                    return 0;
+                });
+
+                console.log(timeIntervals);
+                for (var k = 0; k < timeIntervals.length; k += 1) {
+                    var datePeriod = timeIntervals[k];
                     totalInDateCount += datePeriod.docCount;
-                    $('input[name="daterange"][value="' + k + '"]').parent().find('span').text(datePeriod.docCount);
+                    $('input[name="daterange"][value="' + datePeriod.index + '"]')
+                        .parent()
+                        .find('span')
+                        .text(datePeriod.docCount);
                 }
-                $('input[name="daterange"][value="-1"]').parent().find('span').text(totalInDateCount);
+                $('input[name="daterange"][value="-1"]')
+                    .parent()
+                    .find('span')
+                    .text(totalInDateCount);
 
                 // show/hide load more button
                 if (data.isMore) {
@@ -79,21 +151,21 @@
                     $('#flere').addClass('hidden');
                 }
             },
-            error: function (error) {
+            error: function(error) {
                 console.log(error);
             },
         });
     }
-    function submitForm () {
+    function submitForm() {
         $(this.form).submit();
     }
-    function getMore (e) {
+    function getMore(e) {
         var $c = $('input[name=c]');
         var v = Number($c.val());
         setC(v + 1);
         update(e);
     }
-    function changeFasett (e) {
+    function changeFasett(e) {
         setC(1);
         $('.sokeresultatliste').empty();
         $('.utvidbar.erValgt').removeClass('erValgt');
@@ -105,20 +177,20 @@
         $('.wic').prop('checked', false);
         update(e);
     }
-    function changeFasettMobile (e) {
+    function changeFasettMobile(e) {
         var $facet = $('input[name="f"][value="' + $(this).data('facet') + '"]').eq(0);
         if (!$facet.is(':checked')) {
             $facet.prop('checked', true);
             $facet.trigger('change');
         }
     }
-    function changeDefaultFasett (e) {
+    function changeDefaultFasett(e) {
         setC(1);
         $('.sokeresultatliste').empty();
         $('.wic').prop('checked', false);
         update(e);
     }
-    function changeUnderfasett (e) {
+    function changeUnderfasett(e) {
         setC(1);
         $('.sokeresultatliste').empty();
         if ($('.utvidbar.erValgt div input:checked').length === 0) {
@@ -132,12 +204,12 @@
         }
         update(e);
     }
-    function changeDate (e) {
+    function changeDate(e) {
         setC(1);
         $('.sokeresultatliste').empty();
         update(e);
     }
-    function changeSort (e) {
+    function changeSort(e) {
         setC(1);
         $('.sokeresultatliste').empty();
         $('#fasettform [name="s"]').val($(this).val());
