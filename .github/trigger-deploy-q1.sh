b2871517557bc59581defde81f784539bb37e36d
Tom-Anders Røst <tom.anders.roest@gmail.com>
2020-04-08 14:21:11 +0200

    IV-518, adds same formula to triggers

--- .github/trigger-deploy-q1.sh
+++ .github/trigger-deploy-q1.sh
@@ -1,3 +1,3 @@
-version="$(git tag  | sed 's/.*\(v[0-9].[0-9].[0-9]\).*/\1/p' | sort -V | tail -1)-$(git rev-parse --short HEAD)"
+version="$(git describe --tags `git rev-list --tags --max-count=1`)-$(git rev-parse --short HEAD)"
 git tag -a "${version}" -m "${version}"
 git push && git push --tags
