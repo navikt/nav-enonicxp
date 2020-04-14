6047b9461e4fcd81a908b60683a7cdbd29300660
Tom-Anders Røst <tom.anders.roest@gmail.com>
2020-04-14 14:26:16 +0200

    IV-518, updated the cmd after PR

--- .github/trigger-deploy-q6.sh
+++ .github/trigger-deploy-q6.sh
@@ -1,3 +1,3 @@
-version="$(git tag --sort=committerdate | grep -E '^v[0-9]' | tail -1)-$(git rev-parse --short HEAD)-$(git rev-parse --abbrev-ref HEAD)"
+version="$(git tag | grep -E '^v[0-9]' | grep -v '-' | sort -V | tail -1)-$(git rev-parse --short HEAD)-$(git rev-parse --abbrev-ref HEAD)"
 git tag -a "${version}" -m "${version}"
 git push && git push --tags
