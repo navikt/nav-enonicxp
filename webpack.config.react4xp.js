674dfd72214f1ec4a059de1d0f7294db4cd359b5
Tom-Anders Røst <tom.anders.roest@gmail.com>
2020-08-11 16:24:05 +0200

    adds scaffolding and build for react4xp

---
+++ webpack.config.react4xp.js
@@ -0,0 +1,63 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = function (env, config) {
+    const updatedConfig = config;
+    // This makes 'npm link' symlinks in node_modules work:
+    updatedConfig.resolve.symlinks = true;
+
+    // Compile .scss and friends:
+    updatedConfig.module.rules = [
+        ...(updatedConfig.module.rules || []),
+        {
+            test: /\.less$/,
+            use: [
+                {
+                    loader: 'style-loader',
+                },
+                {
+                    loader: 'css-loader',
+                },
+                {
+                    loader: 'less-loader',
+                    options: {
+                        lessOptions: {
+                            strictMath: true,
+                        },
+                    },
+                },
+            ],
+        },
+        {
+            test: /\.((sa|sc|c))ss$/i,
+            use: [
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader',
+                    options: {
+                        importLoaders: 1,
+                        modules: { auto: true },
+                    },
+                },
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        sassOptions: {
+                            outputStyle: 'compressed',
+                        },
+                    },
+                },
+            ],
+        },
+    ];
+
+    // Set up how the compiled assets are exported:
+    updatedConfig.plugins = [
+        ...(updatedConfig.plugins || []),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].[contenthash:9].css',
+        }),
+    ];
+
+    return updatedConfig;
+};
