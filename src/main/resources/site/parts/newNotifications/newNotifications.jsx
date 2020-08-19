faf459918edfdb941ddcddbcd92d7f0ee06f1310
Tom-Anders Røst <tom.anders.roest@gmail.com>
2020-08-19 09:56:39 +0200

    Adds jsx file to the part for a cleaner setup

---
+++ src/main/resources/site/parts/newNotifications/newNotifications.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+import Notifications from '../../../react4xp/entries/DesignNotifications';
+// import Notifications from '../../../react4xp/entries/Notifications';
+
+export default (props) => <Notifications {...props} />;
