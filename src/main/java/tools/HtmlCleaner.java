d9769ceb39904dade558fe7733df15d5ad47e35b
Tom-Anders Røst <tom.anders.roest@gmail.com>
2021-01-18 11:01:10 +0100

    Adds more cleaning rules

--- src/main/java/tools/HtmlCleaner.java
+++ src/main/java/tools/HtmlCleaner.java
@@ -7,20 +7,18 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.List;
+import java.util.*;
+import java.util.stream.*;
 
 public final class HtmlCleaner {
+    private Boolean isEmpty(Element current) {
+        String content = current.text().trim();
+        return content.equals("nbsp;") || content.isEmpty();
+    }
+
     public String clean(String htmlString) {
         Document doc = Jsoup.parse(htmlString);
 
-        // handle empty h-tags --> p
-        Elements headers = doc.select("h1, h2, h3, h4");
-        for (Element heading : headers) {
-            String content = heading.text();
-            if (content.equals("nbsp;") || content.isEmpty()){
-                heading.tagName("p");
-                heading.text("");
-            }
-        }
         // Remove empty ul- & li-tags
         Elements uls = doc.select("ul");
         for (Element elem: uls) {
@@ -35,8 +33,28 @@ public final class HtmlCleaner {
             }
         }
 
-        // TODO: Remove tabs and linebreaks
-        // TODO: Strip all spaces if children are just text
+        Elements allTags = doc.body().select("*");
+        String[] hTags = {"h1", "h2", "h3", "h4"};
+        for (Element elem: allTags) {
+            if (elem.tagName().equals("body")) {
+                continue;
+            }
+            // convert empty h-tags to p
+            if (Arrays.stream(hTags).anyMatch(elem.tagName()::equals) && this.isEmpty(elem )) {
+                elem.tagName("p");
+                elem.text("");
+            }
+            // strip formatting chars
+            if (!elem.text().equals("")) {
+                String content = elem.text();
+                String cleanContent = content.trim().replace("\n", " ").replace("\r", "").replace("\t", "");
+                elem.text(cleanContent);
+            }
+            // remove any empty tags
+            if (!elem.tagName().equals("p") && elem.childrenSize() == 0 && (!elem.hasText() || this.isEmpty(elem))) {
+                elem.remove();
+            }
+        }
         return doc.body().html();
     }
 }
