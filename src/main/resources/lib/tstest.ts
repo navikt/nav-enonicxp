ea57d059f5fa7b34cd632588bf2bed5e14f1f252
anders <anders@nomerstad.com>
2021-05-12 15:04:03 +0200

    Fikser node_modules resolve

--- src/main/resources/lib/tstest.ts
+++ src/main/resources/lib/tstest.ts
@@ -1,4 +1,5 @@
 import contentLib from '/lib/xp/content';
+import { Content } from 'enonic-types/content';
 import { SectionPage } from '../site/content-types/section-page/section-page';
 import { PageWithSideMenusConfig } from '../site/pages/page-with-side-menus/page-with-side-menus-config';
 
@@ -6,18 +7,16 @@ export const tstest = () => {
   const sectionPage = contentLib.get<SectionPage>({
     key: '/www.nav.no/no/samarbeidspartner',
   });
-  log.info(JSON.stringify(sectionPage));
 
   if (sectionPage) {
     log.info(sectionPage.data.newsContents);
   }
 
-  const productPage = contentLib.get<PageWithSideMenusConfig>({
+  const productPage = contentLib.get<Content<any, PageWithSideMenusConfig, any>>({
     key: '/www.nav.no/no/produktside-test',
   });
-  log.info(JSON.stringify(productPage));
 
   if (productPage) {
-    log.info(productPage.data.leftMenuHeader);
+    log.info(JSON.stringify(productPage.page.config));
   }
 };
