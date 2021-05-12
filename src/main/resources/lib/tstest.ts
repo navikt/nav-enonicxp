65138dba8933cff8cbf150b55b38476016a87168
anders <anders@nomerstad.com>
2021-05-12 15:21:35 +0200

    Tester typeguard

--- src/main/resources/lib/tstest.ts
+++ src/main/resources/lib/tstest.ts
@@ -2,6 +2,14 @@ import contentLib from '/lib/xp/content';
 import { Content } from 'enonic-types/content';
 import { SectionPage } from '../site/content-types/section-page/section-page';
 import { PageWithSideMenusConfig } from '../site/pages/page-with-side-menus/page-with-side-menus-config';
+import { MainArticle } from '../site/content-types/main-article/main-article';
+
+type ContentGetResult = ReturnType<typeof contentLib.get>;
+
+const isMainArticle = (
+  content: ContentGetResult
+): content is ContentGetResult & { data: MainArticle } =>
+  content?.type === `${app.name}:main-article`;
 
 export const tstest = () => {
   const sectionPage = contentLib.get<SectionPage>({
@@ -18,5 +26,13 @@ export const tstest = () => {
 
   if (productPage) {
     log.info(JSON.stringify(productPage.page.config));
+  }
+
+  const nonCoercedContent = contentLib.get({
+    key: '/www.nav.no/no/person/arbeid/arbeidsavklaringspenger/arbeidsavklaringspenger-aap',
+  });
+
+  if (isMainArticle(nonCoercedContent)) {
+    log.info(`It's a fact! ${nonCoercedContent.data.fact}`);
   }
 };
