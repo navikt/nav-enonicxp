f53b4f1a546fcd22bdc19b12719b2aed485aadfa
anders-nom <anders@nomerstad.com>
2021-11-09 14:21:08 +0100

    Fjerner diverse ubrukt funksjonalitet fra gammel frontend og Enonic CMS migrering

--- src/main/resources/assets/js/versionHistory.js
+++
@@ -1,8 +0,0 @@
-var buttons = document.querySelectorAll('.ekspanderbartPanel__hode');
-
-buttons.forEach((el) => {
-    el.onclick = function () {
-        el.parentElement.classList.toggle('ekspanderbartPanel--apen');
-        el.parentElement.classList.toggle('ekspanderbartPanel--lukket');
-    };
-});
