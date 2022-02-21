6148497cbcf050ad5d4fc1968c7ca49ff39d5a1c
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-02-21 20:47:35 +0100

    Fikser parent path trailing slash

--- src/main/resources/admin/widgets/archive-restore/restore/archive-restore-response.ts
+++ src/main/resources/admin/widgets/archive-restore/restore/archive-restore-response.ts
@@ -1,51 +1,61 @@
 import contentLib from '/lib/xp/content';
 import thymeleafLib from '/lib/thymeleaf';
 import { getParentPath } from '../../../../lib/utils/nav-utils';
+import { runInBranchContext } from '../../../../lib/utils/branch-context';
 
 const view = resolve('./archive-restore-response.html');
 
 const restoreFromArchive = (
     selectedContentId: string,
     targetId: string
-): { success: boolean; message: string } => {
-    const targetPath = contentLib.get({ key: targetId })?._path;
-    if (!targetPath) {
-        return { success: false, message: 'Feil: kunne ikke gjenopprette til denne mappen' };
-    }
-
-    // contentLib.restore throws if content does not exist in the archive
-    try {
-        const restoredId = contentLib.restore({
-            content: selectedContentId,
-            path: targetPath,
-        })?.[0];
-        if (!restoredId) {
-            return { success: false, message: 'Feil: det arkiverte innholdet ble ikke funnet' };
+): { success: boolean; message: string } =>
+    runInBranchContext(() => {
+        const targetPath = contentLib.get({ key: targetId })?._path;
+        if (!targetPath) {
+            return { success: false, message: 'Feil: kunne ikke gjenopprette til denne mappen' };
         }
 
-        const restoredContent = contentLib.get({ key: restoredId });
-        if (!restoredContent) {
-            return { success: false, message: 'Feil: gjenoppretting fra arkiv mislykkes' };
+        // contentLib.restore throws if content does not exist in the archive
+        try {
+            const restoredId = contentLib.restore({
+                content: selectedContentId,
+                path: targetPath,
+            })?.[0];
+            if (!restoredId) {
+                return {
+                    success: false,
+                    message: 'Feil: det valgte innholdet ble ikke funnet i arkivet',
+                };
+            }
+
+            const restoredContent = contentLib.get({ key: restoredId });
+            if (!restoredContent) {
+                return { success: false, message: 'Feil: gjenoppretting fra arkiv mislykkes' };
+            }
+
+            // The target path for restore seems to be inconsistently ignored. Workaround for this bug.
+            if (getParentPath(restoredContent._path) !== targetPath) {
+                log.warning(
+                    `Content (${restoredId}) was not restored to the selected path (${targetPath}), fixing...`
+                );
+                contentLib.move({
+                    source: restoredContent._id,
+                    target: `${targetPath}${targetPath.endsWith('/') ? '' : '/'}`,
+                });
+            }
+
+            return {
+                success: true,
+                message: `Gjenoppretting av "${restoredContent.displayName}" var vellykket`,
+            };
+        } catch (e) {
+            log.info(`Archive restore exception: ${e}`);
+            return {
+                success: false,
+                message: 'Feil: det valgte innholdet ble ikke funnet i arkivet',
+            };
         }
-
-        // The target path for restore seems to be inconsistently ignored. Workaround for this bug.
-        if (getParentPath(restoredContent._path) !== targetPath) {
-            log.warning(
-                `Content (${restoredId}) was not restored to the selected path (${targetPath}), fixing...`
-            );
-            contentLib.move({ source: restoredContent._id, target: targetPath });
-        }
-
-        log.info(`Restored from archive: ${JSON.stringify(restoredContent)} -> ${targetPath}`);
-
-        return {
-            success: true,
-            message: `Gjenoppretting av "${restoredContent.displayName}" var vellykket`,
-        };
-    } catch (e) {
-        return { success: false, message: 'Feil:  det arkiverte innholdet ble ikke funnet' };
-    }
-};
+    }, 'draft');
 
 export const archiveRestoreResponse = (req: XP.Request) => {
     const { contentId, selectedContent } = req.params;
