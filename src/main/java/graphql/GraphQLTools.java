c5d7164c5f7a9e6676b68a01635a1bd5a5925694
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-04-08 16:18:30 +0200

    Fjerner Java-eksperiment

--- src/main/java/graphql/GraphQLTools.java
+++
@@ -1,9 +0,0 @@
-package graphql;
-
-import graphql.schema.GraphQLSchema;
-
-public class GraphQLTools {
-    public static void graphQLSchemaReader(GraphQLSchema schema) {
-        System.out.println("Hello!");
-    }
-}
--- src/main/resources/lib/guillotine/queries/sitecontent/build-sitecontent-query.ts
+++ src/main/resources/lib/guillotine/queries/sitecontent/build-sitecontent-query.ts
@@ -3,6 +3,4 @@ import contentLib, { Content } from '/lib/xp/content';
 export const buildSitecontentQuery = (content: Content) => {
     const { type } = content;
 
-
-
 };
