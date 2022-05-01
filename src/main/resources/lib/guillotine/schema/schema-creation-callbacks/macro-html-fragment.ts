b0dc5efce04e5a2efcec7bd14dfb52b2354d674f
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-05-01 22:18:21 +0200

    Refactor av schema creation callbacks for html-fragment v2

--- src/main/resources/lib/guillotine/schema/schema-creation-callbacks/macro-html-fragment.ts
+++ src/main/resources/lib/guillotine/schema/schema-creation-callbacks/macro-html-fragment.ts
@@ -1,125 +1,13 @@
 import contentLib from '/lib/xp/content';
-import graphQlLib, { GraphQLResolver } from '/lib/graphql';
-import { Context } from '/lib/guillotine';
+import graphQlLib from '/lib/graphql';
 import macroLib from '/lib/guillotine/macro';
-import { sanitizeText } from '/lib/guillotine/util/naming';
-import { CreationCallback, graphQlCreateObjectType } from '../../utils/creation-callback-utils';
+import { CreationCallback } from '../../utils/creation-callback-utils';
 import { getKeyWithoutMacroDescription } from '../../../utils/component-utils';
 import { HtmlAreaPartConfig } from '../../../../site/parts/html-area/html-area-part-config';
 
-type MacroDescriptor = {
-    name: string;
-    applicationKey: string;
-    form: any;
-};
-
-const getMacroDescriptors = (applicationsKeys: string[]): MacroDescriptor[] => {
-    const descriptorBean = __.newBean(
-        'com.enonic.lib.guillotine.handler.ComponentDescriptorHandler'
-    );
-    return __.toNativeObject(descriptorBean.getMacroDescriptors(applicationsKeys));
-};
-
-// Generates a special case of the RichText type where the html-fragment macro is not
-// included in the macro config type. This is necessary in order to prevent circular
-// references in the graphql schema, as this macro can itself contain other macros
-// when resolved
-//
-// (this is largely derived from createMacroDataConfigType() in lib-guillotine)
-const generateRichTextTypeWithoutHtmlFragment = (context: Context) => {
-    const macroDescriptors = getMacroDescriptors(context.options.applications as string[]).filter(
-        (descriptor) => descriptor.name !== 'html-fragment'
-    );
-
-    const macroConfigTypeFields = macroDescriptors.reduce(
-        (macroConfigTypeFieldsAcc, descriptor) => {
-            const sanitizeDescriptorName = sanitizeText(descriptor.name);
-
-            const macroTypeName = `Macro_${sanitizeText(
-                descriptor.applicationKey
-            )}_${sanitizeDescriptorName}`;
-
-            const macroDataConfigTypeName = `${macroTypeName}_DataConfig`;
-
-            return {
-                ...macroConfigTypeFieldsAcc,
-                [sanitizeDescriptorName]: {
-                    type: graphQlLib.reference(macroDataConfigTypeName),
-                    resolve: (env) => {
-                        return env.source[descriptor.name];
-                    },
-                },
-            };
-        },
-        {} as Record<string, GraphQLResolver>
-    );
-
-    const macroConfigTypeModified = graphQlCreateObjectType(context, {
-        name: `MacroConfigHtmlFragment`,
-        description: `Macro config type modified for html-fragment`,
-        fields: macroConfigTypeFields,
-    });
-
-    const macroTypeModified = graphQlCreateObjectType(context, {
-        name: `MacroHtmlFragment`,
-        description: `Macro type modified for html-fragment`,
-        fields: {
-            ref: {
-                type: graphQlLib.GraphQLString,
-                resolve: (env) => {
-                    return env.source.ref;
-                },
-            },
-            name: {
-                type: graphQlLib.GraphQLString,
-                resolve: (env) => {
-                    return env.source.name;
-                },
-            },
-            descriptor: {
-                type: graphQlLib.GraphQLString,
-                resolve: (env) => {
-                    return env.source.descriptor;
-                },
-            },
-            config: {
-                type: macroConfigTypeModified,
-                resolve: (env) => {
-                    return env.source.config;
-                },
-            },
-        },
-    });
-
-    return graphQlCreateObjectType(context, {
-        name: 'RichTextHtmlFragment',
-        description: 'RichText type for html-fragment',
-        fields: {
-            processedHtml: {
-                type: graphQlLib.GraphQLString,
-                resolve: (env) => {
-                    return env.source.processedHtml;
-                },
-            },
-            macros: {
-                type: graphQlLib.list(macroTypeModified),
-                resolve: (env) => {
-                    return env.source.macrosAsJson;
-                },
-            },
-            macrosAsJson: {
-                type: graphQlLib.Json,
-                resolve: (env) => {
-                    return env.source.macrosAsJson;
-                },
-            },
-        },
-    });
-};
-
 export const macroHtmlFragmentCallback: CreationCallback = (context, params) => {
     params.fields.processedHtml = {
-        type: generateRichTextTypeWithoutHtmlFragment(context),
+        type: graphQlLib.reference('RichText'),
         resolve: (env) => {
             const { fragmentId } = env.source;
             if (!fragmentId) {
