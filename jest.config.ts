2bbe5a6bf124917951ca5ed024531152f1686fbd
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-05-10 17:16:24 +0200

    Fjerner ubrukt modulename mapping

--- jest.config.ts
+++ jest.config.ts
@@ -106,7 +106,6 @@ export default {
     // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
     moduleNameMapper: {
         '^/lib/(.*)': '<rootDir>/src/main/resources/_test/mocks/lib/$1',
-        '^app$': '<rootDir>/src/main/resources/_test/mocks/appConfig.ts',
     },
 
     // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
