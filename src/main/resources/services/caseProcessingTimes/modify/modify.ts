8dacffb61fefc407ef70a639814c1414f64e8959
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-05-12 16:46:41 +0200

    ctrl+c ctrl+v

---
+++ src/main/resources/services/caseProcessingTimes/modify/modify.ts
@@ -0,0 +1,88 @@
+import nodeLib from '/lib/xp/node';
+import {
+    GlobalValueInput,
+    gvServiceInvalidRequestResponse,
+    validateGlobalValueInputAndGetErrorResponse,
+} from '../utils';
+import { runInBranchContext } from '../../../lib/utils/branch-context';
+import { getGlobalValueSet } from '../../../lib/utils/global-value-utils';
+import { forceArray } from '../../../lib/utils/nav-utils';
+import { GlobalValueItem } from '../../../types/content-types/global-value-set';
+import { logger } from '../../../lib/utils/logging';
+
+const itemNameExists = (valueItems: GlobalValueItem[], itemName: string, key: string) =>
+    itemName && valueItems.find((item) => item.itemName === itemName && item.key !== key);
+
+export const modifyGlobalValueItemService = (req: XP.Request) => {
+    const errorResponse = validateGlobalValueInputAndGetErrorResponse(req.params);
+    if (errorResponse) {
+        return errorResponse;
+    }
+
+    const { contentId, key, itemName, numberValue } = req.params as unknown as GlobalValueInput & {
+        key?: string;
+    };
+
+    if (!key) {
+        return gvServiceInvalidRequestResponse(`Parameter 'key' missing`);
+    }
+
+    const content = runInBranchContext(() => getGlobalValueSet(contentId), 'draft');
+    if (!content) {
+        return gvServiceInvalidRequestResponse(`Global value set with id ${contentId} not found`);
+    }
+
+    const valueItems = forceArray(content.data?.valueItems);
+
+    const itemToModify = valueItems.find((item) => item.key === key);
+    if (!itemToModify) {
+        gvServiceInvalidRequestResponse(`Item with key ${key} not found on ${contentId}`);
+    }
+
+    if (itemName && itemNameExists(valueItems, itemName, key)) {
+        gvServiceInvalidRequestResponse(`Item name ${itemName} already exists on ${contentId}`);
+    }
+
+    try {
+        const repo = nodeLib.connect({
+            repoId: 'com.enonic.cms.default',
+            branch: 'draft',
+        });
+
+        const modifiedItem = {
+            key,
+            itemName,
+            ...(numberValue !== undefined && { numberValue }),
+        };
+
+        repo.modify({
+            key: contentId,
+            editor: (_content) => {
+                _content.data.valueItems = valueItems.map((item) =>
+                    item.key === key ? modifiedItem : item
+                );
+
+                return _content;
+            },
+        });
+
+        return {
+            status: 200,
+            contentType: 'application/json',
+            body: {
+                message: `Successfully modified ${key} on ${contentId}`,
+                level: 'info',
+            },
+        };
+    } catch (e) {
+        logger.critical(`Error modifying ${key} on ${contentId} - ${e}`);
+        return {
+            status: 500,
+            contentType: 'application/json',
+            body: {
+                message: `Error modifying ${key} on ${contentId} - ${e}`,
+                level: 'error',
+            },
+        };
+    }
+};
---
+++ src/main/resources/services/caseProcessingTimes/reorder/reorder.ts
@@ -0,0 +1,79 @@
+import nodeLib from '/lib/xp/node';
+import { forceArray, parseJsonArray } from '../../../lib/utils/nav-utils';
+import { gvServiceInvalidRequestResponse } from '../utils';
+import { getGlobalValueSet } from '../../../lib/utils/global-value-utils';
+import { GlobalValueItem } from '../../../types/content-types/global-value-set';
+import { logger } from '../../../lib/utils/logging';
+
+// Verify that the keys-array from the request matches the keys in the global values set
+const validateKeys = (keysFromParam: string[], valueItems: GlobalValueItem[]) => {
+    return (
+        keysFromParam.length === valueItems.length &&
+        valueItems.every((item) => keysFromParam.includes(item.key))
+    );
+};
+
+export const reorderGlobalValuesService = (req: XP.Request) => {
+    const { contentId, orderedKeys } = req.params;
+
+    if (!orderedKeys || !contentId) {
+        return gvServiceInvalidRequestResponse(
+            `Missing parameters:${!orderedKeys && ' "orderedKeys"'}${!contentId && ' "contentId"'}`
+        );
+    }
+
+    const keysParsed = parseJsonArray(orderedKeys);
+    if (!keysParsed) {
+        return gvServiceInvalidRequestResponse('Required parameter "orderedKeys" is invalid');
+    }
+
+    const content = getGlobalValueSet(contentId);
+    if (!content) {
+        return gvServiceInvalidRequestResponse(`Global value set with id ${contentId} not found`);
+    }
+
+    const items = forceArray(content.data.valueItems);
+    if (!validateKeys(keysParsed, items)) {
+        return gvServiceInvalidRequestResponse(
+            `Keys provided does not match keys in global value set ${contentId}`
+        );
+    }
+
+    const reorderedItems = keysParsed.map((key) => items.find((item) => item.key === key));
+
+    try {
+        const repo = nodeLib.connect({
+            repoId: 'com.enonic.cms.default',
+            branch: 'draft',
+        });
+
+        repo.modify({
+            key: contentId,
+            editor: (_content) => {
+                _content.data.valueItems = reorderedItems;
+
+                return _content;
+            },
+        });
+
+        return {
+            status: 200,
+            contentType: 'application/json',
+            body: {
+                message: `Successfully reordered values on ${contentId}`,
+                level: 'info',
+            },
+        };
+    } catch (e) {
+        const message = `Error reordering values on ${contentId} - ${e}`;
+        logger.error(message);
+        return {
+            status: 500,
+            contentType: 'application/json',
+            body: {
+                message,
+                level: 'error',
+            },
+        };
+    }
+};
