2fc82b4ceabe3b62e26a284f5ee504e3ce010cc4
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-05-16 14:28:17 +0200

    Comments etc

--- src/main/resources/services/globalValues/getSet/getSet.ts
+++ src/main/resources/services/globalValues/getSet/getSet.ts
@@ -10,6 +10,9 @@ export const getGlobalValueSetService = (req: XP.Request) => {
         return gvServiceInvalidRequestResponse(`Global value set with id ${contentId} not found`);
     }
 
+    // The original implementation of this had only one value type (what is now the "numberValue" type),
+    // and therefore did not set the type for each value. Account for this to ensure the frontend receives
+    // the correct type for rendering the editor
     const type = content.type === 'no.nav.navno:global-value-set' ? 'numberValue' : 'caseTime';
     const items = forceArray(content.data?.valueItems).map((item) => ({ ...item, type }));
 
