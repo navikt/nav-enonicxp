bce6de4b74f203f34000c06a6a93a775eb03fb7d
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-06-27 16:49:49 +0200

    Returnerer response til helsesjekk...

--- src/main/resources/lib/controllers/frontend-proxy.ts
+++ src/main/resources/lib/controllers/frontend-proxy.ts
@@ -22,9 +22,18 @@ const errorResponse = (url: string, status: number, message: string) => {
     }
 
     return {
-        contentType: 'text/html',
+        contentType: 'text/html; charset=UTF-8',
         body: `<div>${msg}</div>`,
         status,
+    };
+};
+
+// Prevents outdated health-check from blocking routing to our servers...
+// TODO: remove this asap
+const healthCheckDummyResponse = () => {
+    return {
+        contentType: 'text/html; charset=UTF-8',
+        body: '<!DOCTYPE html><html lang="no"><head><meta charset="utf-8"><title>Nav.no</title></head><body><div>Hello world!</div></body></html>',
     };
 };
 
@@ -49,6 +58,13 @@ export const frontendProxy = (req: XP.Request, path?: string) => {
 
     if (!path) {
         const content = portalLib.getContent();
+
+        log.info(req.url);
+        if (req.url.endsWith('/no/person') && content?.type !== 'no.nav.navno:dynamic-page') {
+            log.info(`Returning dummy response for ${req.url}`);
+            return healthCheckDummyResponse();
+        }
+
         if (!contentTypesForFrontendProxy[content?.type]) {
             return noRenderResponse();
         }
