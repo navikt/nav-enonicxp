042bc9d3a391e08f1a1d55a6aad4e88f8d13d644
perolavm <per.olav.mariussen@nav.no>
2022-08-11 13:06:28 +0200

    Refactor: rekursiv uthenting av interne lenker + mer logging

--- src/main/resources/lib/guillotine/schema/schema-creation-callbacks/internal-link-callback.ts
+++ src/main/resources/lib/guillotine/schema/schema-creation-callbacks/internal-link-callback.ts
@@ -1,35 +1,42 @@
 import contentLib from '/lib/xp/content';
-import graphQlLib from '/lib/graphql';
 import { logger } from '../../../utils/logging';
-import { CreationCallback, graphQlCreateObjectType } from '../../utils/creation-callback-utils';
+import { CreationCallback } from '../../utils/creation-callback-utils';
 
 export const internalLinkCallback: CreationCallback = (context: any, params) => {
 
-    const internalLinkUrl = graphQlCreateObjectType(context, {
-        name: context.uniqueName('resolvedInternalLink'),
-        description: 'resolvedInternalLink',
-        fields: {
-            targetUrl: { type: graphQlLib.GraphQLString }
-        },
-    });
+    logger.info('internalLinkCallback');
 
-    // Resolve targetUrl
-    params.fields.targetUrl = {
-        args: { contentId: graphQlLib.GraphQLID },
-        type: internalLinkUrl,
-        resolve: (env) => {
-            const { contentId } = env.args;
-            if (!contentId) {
-                logger.error('No contentId provided for internal-link resolver');
-                return undefined;
-            }
-            const content = contentLib.get({key: contentId});
-            if (!content) {
-                logger.error(`Content not found for internal-link id ${contentId}`);
-                return undefined;
-            }
-            logger.info(`targetUrl: ${content._path}`)
-            return content._path;
+    const getTarget = (contentId: string): contentLib.Content | null => {
+        if (!contentId) {
+            return null;
         }
+        let content = contentLib.get({key: contentId});
+        if (!content) {
+            return null;
+        }
+        // Keep following internal-links to get final target
+        if (content.type === 'no.nav.navno:internal-link') {
+            if (!content.data.target) {
+                return null;
+            }
+            content = getTarget(content.data.target);
+        }
+        return content;
+    };
+
+    // Resolve target
+    params.fields.data.resolve = (env) => {
+        const { contentId } = env.args;
+        if (!contentId) {
+            logger.error('No contentId provided for internal-link resolver');
+            return undefined;
+        }
+        const content = getTarget(contentId);
+        if (!content) {
+            logger.error(`Content not found for internal-link id ${contentId}`);
+            return undefined;
+        }
+        logger.info(`target: ${content.data}`);
+        return content.data;
     }
 };
