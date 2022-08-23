eef19a7b28b9a356305b7ac966724863af4978d7
perolavm <per.olav.mariussen@nav.no>
2022-08-23 10:39:02 +0200

    graphQL forenkling etter bistand fra Anders :-)

--- src/main/resources/lib/guillotine/schema/schema-creation-callbacks/internal-link-callback.ts
+++ src/main/resources/lib/guillotine/schema/schema-creation-callbacks/internal-link-callback.ts
@@ -1,45 +1,28 @@
 import contentLib from '/lib/xp/content';
 import graphQlLib from '/lib/graphql';
 import { logger } from '../../../utils/logging';
-import { CreationCallback, graphQlCreateObjectType } from '../../utils/creation-callback-utils';
+import { CreationCallback } from '../../utils/creation-callback-utils';
 
 export const internalLinkCallback: CreationCallback = (context, params) => {
 
-    let count = 0;
-
-    const publish = graphQlCreateObjectType(context, {
-        name: context.uniqueName('Publish'),
-        description: 'Publish',
-        fields: {
-            first: { type: graphQlLib.DateTime },
-            from: { type: graphQlLib.DateTime },
-        },
-    });
-
-    const internalLinkUrl = graphQlCreateObjectType(context, {
-        name: context.uniqueName('resolvedInternalLink'),
-        description: 'resolvedInternalLink',
-        fields: {
-            __typename: { type: graphQlLib.GraphQLString },
-            _id: { type: graphQlLib.GraphQLString },
-            _path: { type: graphQlLib.GraphQLString },
-            type: { type: graphQlLib.GraphQLString },
-            created: { type: graphQlLib.DateTime },
-            modifiedTime: { type: graphQlLib.DateTime },
-            displayName: { type: graphQlLib.GraphQLString },
-            language: { type: graphQlLib.GraphQLString },
-            publish: { type: publish },
-        },
-    });
+    let count = 0; // For max-depth check
 
     const getTarget = (contentId: string): contentLib.Content | null => {
+
+        count++;
+        if (count > 10) {
+            logger.error('Max depth (10) reached for internal-link resolver');
+            return null;
+        }
         if (!contentId) {
             return null;
         }
+
         let content = contentLib.get({key: contentId});
         if (!content) {
             return null;
         }
+
         // Keep following internal-links to get final target
         if (content.type === 'no.nav.navno:internal-link') {
             if (!content.data.target) {
@@ -50,25 +33,21 @@ export const internalLinkCallback: CreationCallback = (context, params) => {
         return content;
     };
 
-    logger.info(`internalLinkCallback: count=[${count}]`);
     // Resolve final target
     params.fields.target = {
-        args: { contentId: graphQlLib.GraphQLID },
-        type: internalLinkUrl,
+        type: graphQlLib.reference('Content'),
         resolve: (env) => {
-            count++;
-            const {contentId} = env.args;
-            logger.info(`internalLinkCallback[${count}]: contentID=${contentId}`);
-            if (!contentId) {
-                logger.error('No contentId provided for internal-link resolver');
-                return undefined;
+            const { target } = env.source;
+            if (!target) {
+                logger.error('No valid target provided for internal-link resolver');
+                return null;
             }
-            const content = getTarget(contentId);
+            const content = getTarget(target);
             if (!content) {
-                logger.error(`Content not found for internal-link id ${contentId}`);
-                return undefined;
+                logger.error(`Content not found for internal-link resolver target=${target}`);
+                return null;
             }
-            logger.info(`internalLinkCallback: final target=[${JSON.stringify(content, null, 2)}]`);
+            logger.info(`internalLinkCallback: final target path=[${content._path}]`);
             return content;
         }
     }
