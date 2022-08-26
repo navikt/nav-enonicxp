c03c1921d5ab331ecd589f89c6bd49eb197e284b
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-08-26 12:58:13 +0200

    Setter inn custompath urler i processedHtml

--- src/main/resources/lib/guillotine/schema/schema-creation-callbacks/richtext.ts
+++ src/main/resources/lib/guillotine/schema/schema-creation-callbacks/richtext.ts
@@ -1,5 +1,12 @@
+import contentLib from '/lib/xp/content';
 import striptags from '/assets/striptags/3.1.1/src/striptags';
 import { CreationCallback } from '../../utils/creation-callback-utils';
+import { hasValidCustomPath } from '../../../custom-paths/custom-paths';
+
+type Links = {
+    contentId: string;
+    linkRef: string;
+};
 
 const macroTagName = 'editor-macro';
 
@@ -7,12 +14,33 @@ const linebreakFilter = new RegExp(/(\r\n|\n|\r|\s)/g);
 
 const macroTagFilter = new RegExp(`<${macroTagName}[^>]*>(.*?)</${macroTagName}>`, 'g');
 
+const insertCustomPaths = (processedHtml: string, links: Links[]) =>
+    links.reduce((html, link) => {
+        const { contentId, linkRef } = link;
+        if (!contentId || !linkRef) {
+            log.info(`Missing ${contentId} ${linkRef}`);
+            return html;
+        }
+
+        const content = contentLib.get({ key: contentId });
+        if (!content || !hasValidCustomPath(content)) {
+            return html;
+        }
+
+        const { customPath } = content.data;
+
+        return html.replace(
+            new RegExp(`<a href="[^"]*" data-link-ref="${linkRef}"`, 'g'),
+            `<a href="${customPath}"`
+        );
+    }, processedHtml);
+
 export const richTextCallback: CreationCallback = (context, params) => {
     params.fields.processedHtml.resolve = (env) => {
-        const { processedHtml } = env.source;
+        const { processedHtml, links } = env.source;
 
         return processedHtml
-            ? processedHtml
+            ? insertCustomPaths(processedHtml, links)
                   // Strip linebreaks, as it may cause errors in the frontend parser
                   .replace(linebreakFilter, ' ')
                   // Strip html tags from the body of macro-tags. Fixes invalid html-nesting caused by the CS editor
