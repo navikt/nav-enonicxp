2fd21c8b234ea1b2a4fa3f580d54a11d52479b49
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-09-20 10:51:21 +0200

    Finner opprinnelig content-type for internal-link ved requests fra draft

--- src/main/resources/lib/guillotine/schema/schema-creation-callbacks/internal-link-callback.ts
+++ src/main/resources/lib/guillotine/schema/schema-creation-callbacks/internal-link-callback.ts
@@ -1,21 +1,28 @@
-import contentLib from '/lib/xp/content';
+import contentLib, { Content } from '/lib/xp/content';
+import nodeLib from '/lib/xp/node';
+import graphQlLib from '/lib/graphql';
+import contextLib from '/lib/xp/context';
 import { logger } from '../../../utils/logging';
 import { CreationCallback } from '../../utils/creation-callback-utils';
+import { getNodeVersions } from '../../../utils/version-utils';
+import { appDescriptor, contentRepo } from '../../../constants';
 
-export const internalLinkCallback: CreationCallback = (context, params) => {
+const internalLinkContentType = `${appDescriptor}:internal-link`;
 
-    const getTarget = (contentId: string, count: number): contentLib.Content | null => {
-
+export const internalLinkDataCallback: CreationCallback = (context, params) => {
+    const getTarget = (contentId: string, count: number): Content | null => {
         count++;
         if (count > 10) {
-            logger.critical(`internalLinkCallback: Max depth (10)/redirect loop - ContentId=${contentId}`);
+            logger.critical(
+                `internalLinkCallback: Max depth (10)/redirect loop - ContentId=${contentId}`
+            );
             return null;
         }
         if (!contentId) {
             return null;
         }
 
-        let content = contentLib.get({key: contentId});
+        let content = contentLib.get({ key: contentId });
         if (!content) {
             return null;
         }
@@ -43,5 +50,55 @@ export const internalLinkCallback: CreationCallback = (context, params) => {
             return null;
         }
         return content;
-    }
+    };
+};
+
+export const internalLinkCallback: CreationCallback = (context, params) => {
+    // Find the original content type for the internal-link.
+    //
+    // Old content is sometimes converted to an internal-link in order to redirect to newer content.
+    // We use this originalType-field in the frontend to show a warning in Content Studio that the
+    // content was originally a different type, which may have had content that should be retained
+    // for archival purposes
+    params.fields.originalType = {
+        type: graphQlLib.GraphQLString,
+        resolve: (env) => {
+            if (contextLib.get().branch !== 'draft') {
+                return null;
+            }
+
+            const versions = getNodeVersions({
+                nodeKey: env.source._id,
+                repo: nodeLib.connect({
+                    repoId: contentRepo,
+                    branch: 'draft',
+                }),
+                branch: 'draft',
+            });
+
+            const firstVersion = versions[versions.length - 1];
+
+            const firstContent = contentLib.get({
+                key: firstVersion.nodeId,
+                versionId: firstVersion.versionId,
+            });
+
+            if (!firstContent) {
+                logger.error(
+                    `Could not get first version of content node ${firstVersion.nodeId} - ${firstVersion.versionId}`
+                );
+                return null;
+            }
+
+            const { type } = firstContent;
+
+            if (type === internalLinkContentType) {
+                return null;
+            }
+
+            const typeProps = contentLib.getType(type);
+
+            return typeProps?.displayName || null;
+        },
+    };
 };
