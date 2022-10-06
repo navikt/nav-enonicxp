ddc6274030a8e333f33b336b0de002a4f6e13bbf
perolavm <per.olav.mariussen@nav.no>
2022-10-06 11:01:15 +0200

    testing

--- src/main/resources/lib/guillotine/schema/schema-creation-callbacks/internal-link-callback.ts
+++ src/main/resources/lib/guillotine/schema/schema-creation-callbacks/internal-link-callback.ts
@@ -1,5 +1,5 @@
 import contentLib, { Content } from '/lib/xp/content';
-import graphQlLib from '/lib/graphql';
+//import graphQlLib from '/lib/graphql';
 import { logger } from 'lib/utils/logging';
 import { CreationCallback } from 'lib/guillotine/utils/creation-callback-utils';
 import { insertOriginalContentTypeField } from './common/original-content-type';
@@ -38,18 +38,19 @@ export const internalLinkDataCallback: CreationCallback = (context, params) => {
     };
 
     // Resolve final target
-    params.fields.target.args = { baseContentId: graphQlLib.GraphQLID };
+    //params.fields.target.args = { baseContentId: graphQlLib.GraphQLID };
     params.fields.target.resolve = (env) => {
         const { target } = env.source;
-        const { baseContentId } = env.args;
+        //const { baseContentId } = env.args;
+        const baseContentId = '*dummy*';
         log.info(`internalLinkCallback: ${baseContentId}`);
         if (!target) {
-            logger.error(`internalLinkCallback: No valid target provided - ${baseContentId}`);
+            logger.error(`internalLinkCallback: No valid target provided in ${baseContentId}`);
             return null;
         }
         const content = getTarget(baseContentId, target, 0);
         if (!content) {
-            logger.content(`internalLinkCallback: Content not found - ${baseContentId}`);
+            logger.content(`internalLinkCallback: Content not found in ${baseContentId}`);
             return null;
         }
         return content;
