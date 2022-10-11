3108458db54cdd77dc50da64f3c94da24bcd3c51
perolavm <per.olav.mariussen@nav.no>
2022-10-11 11:04:57 +0200

    [content] på kritisk feil ved redirect loop

--- src/main/resources/lib/guillotine/schema/schema-creation-callbacks/internal-link-callback.ts
+++ src/main/resources/lib/guillotine/schema/schema-creation-callbacks/internal-link-callback.ts
@@ -9,7 +9,11 @@ export const internalLinkDataCallback: CreationCallback = (context, params) => {
         count++;
         if (count > 10) {
             logger.critical(
-                `internalLinkCallback: Max depth (10)/redirect loop - baseContentId=${baseContentId} - contentId=${contentId}`
+                `internalLinkCallback: Max depth (10)/redirect loop 
+                - baseContentId=${baseContentId} 
+                - contentId=${contentId}`,
+                false,
+                true
             );
             return null;
         }
