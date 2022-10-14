44a4699bc83a39d82dc498e29f8be0dd649f8101
perolavm <per.olav.mariussen@nav.no>
2022-10-14 08:55:40 +0200

    "content" -> "editorial" + level FØR "editorial"

--- src/main/resources/lib/utils/logging.ts
+++ src/main/resources/lib/utils/logging.ts
@@ -2,11 +2,11 @@ import contextLib from '/lib/xp/context';
 
 type LogLevel = 'info' | 'warning' | 'error';
 
-const checkContextAndLog = (msg: string, level: LogLevel, logAsInfoInDraftContext?: boolean, content?: boolean) => {
+const checkContextAndLog = (level: LogLevel, msg: string, logAsInfoInDraftContext?: boolean, content?: boolean) => {
     if (logAsInfoInDraftContext && contextLib.get()?.branch === 'draft') {
         log.info(msg);
     } else {
-        log[level](`${content?'[content]':''}${msg}`);
+        log[level](`'[${level}]'${content?'[editorial]':''}' '${msg}`);
     }
 };
 
@@ -15,15 +15,15 @@ const logInfo = (msg: string) => {
 };
 
 const logWarning = (msg: string, logAsInfoInDraftContext?: boolean, content?: boolean) => {
-    checkContextAndLog(`[warning] ${msg}`, 'warning', logAsInfoInDraftContext, content);
+    checkContextAndLog('warning', msg, logAsInfoInDraftContext, content);
 };
 
 const logError = (msg: string, logAsInfoInDraftContext?: boolean, content?: boolean) => {
-    checkContextAndLog(`[error] ${msg}`, 'error', logAsInfoInDraftContext, content);
+    checkContextAndLog('error', msg, logAsInfoInDraftContext, content);
 };
 
 const logCriticalError = (msg: string, logAsInfoInDraftContext?: boolean, content?: boolean) => {
-    checkContextAndLog(`[critical] ${msg}`, 'error', logAsInfoInDraftContext, content);
+    checkContextAndLog('error', msg, logAsInfoInDraftContext, content);
 };
 
 export const logger = {
