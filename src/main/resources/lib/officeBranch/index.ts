93c748bdc5e2061fe42a1c678aca6941a3f7c2ba
Terje Karlsen <terjeofnorway@gmail.com>
2022-11-21 20:55:36 +0100

    Oppdaterer importlenke for v2 av NORG-endepunkt for kontorsider

--- src/main/resources/lib/officeBranch/index.ts
+++ src/main/resources/lib/officeBranch/index.ts
@@ -70,7 +70,7 @@ export const createOfficeBranchFetchSchedule = () => {
         jobDescription: 'Fetches office branches from norg and updates into XP as hourly schedule',
         jobSchedule: {
             type: 'CRON',
-            value: '05 * * * *',
+            value: '15 * * * *',
             timeZone: 'GMT+2:00',
         },
         taskDescriptor: officeBranchFetchTaskName,
