1cb3648b83174b148192c0db37bc8ca74ae05a38
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-12-12 16:33:38 +0100

    Prøver noe...

---
+++ src/main/resources/lib/initApp.ts
@@ -0,0 +1,52 @@
+import './polyfills';
+
+import clusterLib from '/lib/xp/cluster';
+import { updateClusterInfo } from './utils/cluster-utils';
+import { startReliableEventAckListener } from './events/reliable-custom-events';
+import { activateCacheEventListeners } from './cache/invalidate-event-handlers';
+import {
+    activateSitemapDataUpdateEventListener,
+    generateSitemapDataAndActivateSchedule,
+} from './sitemap/sitemap';
+import { activateContentListItemUnpublishedListener } from './contentlists/remove-unpublished';
+import { activateCustomPathNodeListeners } from './custom-paths/event-listeners';
+import { activateSearchIndexEventHandlers } from './search/eventHandlers';
+import { hookLibsWithTimeTravel } from './time-travel/time-travel-hooks';
+import { timeTravelConfig } from './time-travel/time-travel-config';
+import { initSearchRepo } from './search/repo';
+import { startFailsafeSchedule } from './scheduling/scheduler-failsafe';
+import { startOfficeInfoPeriodicUpdateSchedule } from './officeInformation';
+import { createOfficeBranchFetchSchedule } from './officeBranch';
+import { logger } from './utils/logging';
+
+let didInit = false;
+
+export const initApp = () => {
+    if (didInit) {
+        logger.critical('initApp should only run once!');
+        return;
+    }
+
+    didInit = true;
+
+    updateClusterInfo();
+
+    startReliableEventAckListener();
+    activateCacheEventListeners();
+    activateSitemapDataUpdateEventListener();
+    activateContentListItemUnpublishedListener();
+    activateCustomPathNodeListeners();
+    activateSearchIndexEventHandlers();
+
+    hookLibsWithTimeTravel(timeTravelConfig);
+
+    if (clusterLib.isMaster()) {
+        log.info('Running master only init scripts');
+        initSearchRepo();
+        startFailsafeSchedule();
+        generateSitemapDataAndActivateSchedule();
+        startOfficeInfoPeriodicUpdateSchedule();
+        // Todo: Activate this only when we're going live with the new office branch.
+        createOfficeBranchFetchSchedule();
+    }
+};
--- src/main/resources/main.ts
+++ src/main/resources/main.ts
@@ -1,45 +1,8 @@
+import { initApp } from './lib/initApp';
+
 log.info('Started running main');
 
-import './lib/polyfills';
-
-import clusterLib from '/lib/xp/cluster';
-import { startOfficeInfoPeriodicUpdateSchedule } from './lib/officeInformation';
-import { activateCacheEventListeners } from './lib/cache/invalidate-event-handlers';
-import {
-    activateSitemapDataUpdateEventListener,
-    generateSitemapDataAndActivateSchedule,
-} from './lib/sitemap/sitemap';
-import { startReliableEventAckListener } from './lib/events/reliable-custom-events';
-import { updateClusterInfo } from './lib/utils/cluster-utils';
-import { activateContentListItemUnpublishedListener } from './lib/contentlists/remove-unpublished';
-import { startFailsafeSchedule } from './lib/scheduling/scheduler-failsafe';
-import { activateCustomPathNodeListeners } from './lib/custom-paths/event-listeners';
-import { createOfficeBranchFetchSchedule } from 'lib/officeBranch';
-import { activateSearchIndexEventHandlers } from './lib/search/eventHandlers';
-import { hookLibsWithTimeTravel } from './lib/time-travel/time-travel-hooks';
-import { timeTravelConfig } from './lib/time-travel/time-travel-config';
-import { initSearchRepo } from './lib/search/repo';
-
-updateClusterInfo();
-
-startReliableEventAckListener();
-activateCacheEventListeners();
-activateSitemapDataUpdateEventListener();
-activateContentListItemUnpublishedListener();
-activateCustomPathNodeListeners();
-activateSearchIndexEventHandlers();
-
-hookLibsWithTimeTravel(timeTravelConfig);
-
-if (clusterLib.isMaster()) {
-    log.info('Running master only init scripts');
-    initSearchRepo();
-    startFailsafeSchedule();
-    generateSitemapDataAndActivateSchedule();
-    startOfficeInfoPeriodicUpdateSchedule();
-    // Todo: Activate this only when we're going live with the new office branch.
-    createOfficeBranchFetchSchedule();
-}
+initApp();
 
 log.info('Finished running main');
 
