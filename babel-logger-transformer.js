8e2c880128d48ea232a89235aeca99db1b6fba7c
Anders Bråthen Nomerstad <anders@nomerstad.com>
2023-01-06 09:10:01 +0100

    Refactor plugin

--- babel-logger-transformer.js
+++ babel-logger-transformer.js
@@ -1,32 +1,36 @@
+// Injects nashorn globals for filename and line numbers as arguments for our custom logger functions
+
 const transformLogger = ({ types: t }) => {
-    class BabelInlineImport {
+    class BabelTransformLogger {
         constructor() {
             return {
                 visitor: {
                     CallExpression: {
-                        exit(path, state) {
-                            const node = path.node;
-                            if (node.callee.object && node.callee.object.name === 'logger') {
-                                if (node.callee.property.name === 'info') {
-                                    if (!node.arguments[1]) {
-                                        node.arguments[1] = t.identifier('__FILE__');
-                                    }
-                                    if (!node.arguments[2]) {
-                                        node.arguments[2] = t.identifier('__LINE__');
-                                    }
-                                } else {
-                                    if (!node.arguments[1]) {
-                                        node.arguments[1] = t.identifier('undefined');
-                                    }
-                                    if (!node.arguments[2]) {
-                                        node.arguments[2] = t.identifier('undefined');
-                                    }
-                                    if (!node.arguments[3]) {
-                                        node.arguments[3] = t.identifier('__FILE__');
-                                    }
-                                    if (!node.arguments[4]) {
-                                        node.arguments[4] = t.identifier('__LINE__');
-                                    }
+                        exit(path) {
+                            const { node } = path;
+                            if (!node.callee.object || node.callee.object.name !== 'logger') {
+                                return;
+                            }
+
+                            if (node.callee.property.name === 'info') {
+                                if (!node.arguments[1]) {
+                                    node.arguments[1] = t.identifier('__FILE__');
+                                }
+                                if (!node.arguments[2]) {
+                                    node.arguments[2] = t.identifier('__LINE__');
+                                }
+                            } else {
+                                if (!node.arguments[1]) {
+                                    node.arguments[1] = t.identifier('undefined');
+                                }
+                                if (!node.arguments[2]) {
+                                    node.arguments[2] = t.identifier('undefined');
+                                }
+                                if (!node.arguments[3]) {
+                                    node.arguments[3] = t.identifier('__FILE__');
+                                }
+                                if (!node.arguments[4]) {
+                                    node.arguments[4] = t.identifier('__LINE__');
                                 }
                             }
                         },
@@ -36,7 +40,7 @@ const transformLogger = ({ types: t }) => {
         }
     }
 
-    return new BabelInlineImport();
+    return new BabelTransformLogger();
 };
 
 module.exports = transformLogger;
