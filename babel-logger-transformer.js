6cb6c64dda1d26f904fb73cab77ac0d44d4ff300
Anders Bråthen Nomerstad <anders@nomerstad.com>
2023-01-09 09:06:02 +0100

    Bruker linjenummer fra typescript for logging

--- babel-logger-transformer.js
+++ babel-logger-transformer.js
@@ -1,4 +1,5 @@
-// Injects nashorn globals for filename and line numbers as arguments for our custom logger functions
+// Injects nashorn global for filename and the typescript line number
+// as arguments for our custom logger functions
 
 const transformLogger = ({ types: t }) => {
     class BabelTransformLogger {
@@ -12,12 +13,14 @@ const transformLogger = ({ types: t }) => {
                                 return;
                             }
 
+                            const line = node.loc.start.line.toString();
+
                             if (node.callee.property.name === 'info') {
                                 if (!node.arguments[1]) {
                                     node.arguments[1] = t.identifier('__FILE__');
                                 }
                                 if (!node.arguments[2]) {
-                                    node.arguments[2] = t.identifier('__LINE__');
+                                    node.arguments[2] = t.stringLiteral(line);
                                 }
                             } else {
                                 if (!node.arguments[1]) {
@@ -30,7 +33,7 @@ const transformLogger = ({ types: t }) => {
                                     node.arguments[3] = t.identifier('__FILE__');
                                 }
                                 if (!node.arguments[4]) {
-                                    node.arguments[4] = t.identifier('__LINE__');
+                                    node.arguments[4] = t.stringLiteral(line);
                                 }
                             }
                         },
