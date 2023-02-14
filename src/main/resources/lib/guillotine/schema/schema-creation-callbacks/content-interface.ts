259bff3e0ee0096690fd24e6334fa27d2303c250
Anders Bråthen Nomerstad <anders@nomerstad.com>
2023-02-14 15:42:19 +0100

    Comments + logging

--- src/main/resources/lib/guillotine/schema/schema-creation-callbacks/content-interface.ts
+++ src/main/resources/lib/guillotine/schema/schema-creation-callbacks/content-interface.ts
@@ -1,8 +1,8 @@
 import * as contextLib from '/lib/xp/context';
 import { CreationCallback } from '../../utils/creation-callback-utils';
-import { logger } from '../../../utils/logging';
 import { forceArray } from '../../../utils/nav-utils';
 import { getLayersData } from '../../../localization/layers-data';
+import { logger } from '../../../utils/logging';
 
 export const contentInterfaceCallback: CreationCallback = (context, params) => {
     params.fields._path.resolve = (env) => {
@@ -11,16 +11,26 @@ export const contentInterfaceCallback: CreationCallback = (context, params) => {
         const baseLocalePath = data?.customPath || _path;
 
         const { defaultLocale } = getLayersData();
+
+        // For the default locale, we always use the base path
         if (language === defaultLocale) {
             return baseLocalePath;
         }
 
         const isLocalized = !forceArray(inherit).includes('CONTENT');
+
+        // If the content is not localized, we always use the base path
+        // (will resolve to the default layer version)
         if (!isLocalized) {
             return baseLocalePath;
         }
 
         const locale = contextLib.get()?.attributes?.locale;
+
+        // If this happens there is a bug somewhere. Locale context should always be set at this point
+        if (!locale) {
+            logger.critical(`Locale was not set in context for request to ${_path}`);
+        }
 
         return locale ? `${baseLocalePath}/${locale}` : baseLocalePath;
     };
