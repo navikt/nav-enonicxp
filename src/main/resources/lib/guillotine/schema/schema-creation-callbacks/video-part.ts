beb1ad95de3439a884688d1fb8266cbce05836f7
Terje Karlsen <terjeofnorway@gmail.com>
2023-03-02 09:27:25 +0100

    Legger til håndtering og varsling ved lastefeil fra Qbrick

--- src/main/resources/lib/guillotine/schema/schema-creation-callbacks/video-part.ts
+++ src/main/resources/lib/guillotine/schema/schema-creation-callbacks/video-part.ts
@@ -1,33 +1,111 @@
-import httpClient from '/lib/http-client';
+const httpClientLib = require('/lib/http-client');
+import graphQlLib from '/lib/graphql';
 
-import { CreationCallback } from '../../utils/creation-callback-utils';
+import { CreationCallback, graphQlCreateObjectType } from '../../utils/creation-callback-utils';
 import { queryParamsToObject } from '../../../utils/nav-utils';
+import { QbrickMeta, Resource } from 'types/qbrickMeta';
+import { logger } from '../../../utils/logging';
+
+// Todo: Skrive om disse to funksjonene for bedre gjenbruk. De er nesten like.
+const findImageUrlFromVideoMeta = (qbrickMediaData: QbrickMeta) => {
+    const resources = qbrickMediaData?.asset?.resources;
+    if (!resources) {
+        return null;
+    }
+    const firstImageResource = resources?.find((resource: Resource) => resource.type === 'image');
+    if (!firstImageResource) {
+        return null;
+    }
+
+    const smallestRendition = firstImageResource.renditions?.find(
+        (rendition) => rendition.width === 256
+    );
+
+    if (!smallestRendition) {
+        return null;
+    }
+
+    return smallestRendition.links?.find((link) => link.mimeType === 'image/jpg')?.href || null;
+};
+
+const findVideoDurationFromMeta = (qbrickMediaData: QbrickMeta) => {
+    const resources = qbrickMediaData?.asset?.resources;
+    if (!resources) {
+        return 0;
+    }
+    const firstVideoResource = resources?.find((resource: Resource) => resource.type === 'video');
+    if (!firstVideoResource) {
+        return 0;
+    }
+
+    const firstRentition = firstVideoResource.renditions?.find(
+        (rendition) => rendition.type === 'video'
+    );
+
+    if (!firstRentition) {
+        return 0;
+    }
+
+    const firstVideo = firstRentition.videos?.[0];
+    if (!firstVideo) {
+        return 0;
+    }
+    return firstVideo.duration;
+};
 
 export const videoCallback: CreationCallback = (context, params) => {
-    log.info('videoCallback');
-    params.fields.video.resolve = (env) => {
-        const { video } = env.source;
-        if (!video) {
-            return null;
-        }
-
-        const { accountId, mediaId } = queryParamsToObject(video);
-
-        const qbrickImageUrl = `https://video.qbrick.com/api/v1/image/accounts/${accountId}/medias/${mediaId}/snapshot.jpg`;
-
-        log.info(qbrickImageUrl);
-        const response = httpClient.request({
-            url: qbrickImageUrl,
-            method: 'GET',
-            headers: {
-                'Cache-Control': 'no-cache',
+    params.fields.video = {
+        type: graphQlCreateObjectType(context, {
+            name: context.uniqueName('VideoMeta'),
+            description: 'VideoMeta',
+            fields: {
+                videoUrl: { type: graphQlLib.GraphQLString },
+                imageUrl: { type: graphQlLib.GraphQLString },
+                duration: { type: graphQlLib.GraphQLString },
             },
-            connectionTimeout: 20000,
-            readTimeout: 10000,
-            contentType: 'image/jpeg',
-        });
+        }),
+        resolve: (env) => {
+            const videoUrl = env.source.video;
+            if (!videoUrl) {
+                return null;
+            }
 
-        log.info(JSON.stringify(response.bodyStream));
-        return env.source.video;
+            const { accountId, mediaId } = queryParamsToObject(videoUrl);
+            const qbrickMediaData = `https://video.qbrick.com/api/v1/public/accounts/${accountId}/medias/${mediaId}`;
+
+            try {
+                const response: any = httpClientLib.request({
+                    url: qbrickMediaData,
+                    method: 'GET',
+                    contentType: 'application/json',
+                    connectionTimeout: 1000,
+                    readTimeout: 1000,
+                });
+
+                if (response.status !== 200) {
+                    throw new Error('Could not fetch video meta data');
+                }
+
+                log.info(JSON.stringify(response));
+
+                const qbrikMeta: QbrickMeta = JSON.parse(response.body);
+
+                const imageUrl = findImageUrlFromVideoMeta(qbrikMeta);
+                const duration = findVideoDurationFromMeta(qbrikMeta);
+
+                return {
+                    videoUrl,
+                    imageUrl,
+                    duration,
+                };
+            } catch (e) {
+                logger.warning('Could not load qbrick video meta data within 1 second.');
+                return {
+                    videoUrl,
+                    imageUrl: null,
+                    duration: null,
+                };
+            }
+        },
     };
 };
