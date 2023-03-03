18491e89871d67effb3f802d0746d39b7b2b7ebb
Anders Bråthen Nomerstad <anders@nomerstad.com>
2023-03-03 08:56:22 +0100

    Refactor getContentFromAllLayers query

--- src/main/resources/lib/localization/layers-events.ts
+++ src/main/resources/lib/localization/layers-events.ts
@@ -3,98 +3,10 @@ import { EnonicEvent } from '/lib/xp/event';
 import * as clusterLib from '/lib/xp/cluster';
 import * as nodeLib from '/lib/xp/node';
 import { logger } from '../utils/logging';
-import { getLayersData } from './layers-data';
-import { removeDuplicates } from '../utils/nav-utils';
-import { hasValidCustomPath } from '../custom-paths/custom-paths';
-import { buildLocalePath, getContentFromAllLayers } from './locale-utils';
+import { getContentFromAllLayers } from './locale-utils';
 import { CONTENT_ROOT_REPO_ID } from '../constants';
 
 let hasSetupListeners = false;
-
-const isLocalized = (content: any) => content.data.isProcessedForLocalization;
-const setIsLocalized = (content: any) => (content.data.isProcessedForLocalization = true);
-
-// This is not in use... and may never be
-// Depending on how we decide to handle localized urls, this may be removed. The current
-// implementation does not explicitly set localized urls in locale layers, but instead resolved
-// urls with locale suffixes to content from the matching layer
-const processContentOnLocalization = (event: EnonicEvent) => {
-    if (!clusterLib.isMaster()) {
-        return;
-    }
-
-    const { defaultLocale, repoIdToLocaleMap } = getLayersData();
-
-    removeDuplicates(event.data.nodes, (a, b) => a.id === b.id).forEach((node) => {
-        const { id, branch, repo } = node;
-        if (branch !== 'draft') {
-            return;
-        }
-
-        const locale = repoIdToLocaleMap[repo];
-
-        if (!locale) {
-            logger.info(`Repo ${repo} does not contain a layer`);
-            return;
-        }
-
-        if (locale === defaultLocale) {
-            logger.info(`Repo ${repo} contains the default layer`);
-            return;
-        }
-
-        const repoConnection = nodeLib.connect({
-            branch: 'draft',
-            repoId: repo,
-            user: {
-                login: 'su',
-            },
-            principals: ['role:system.admin'],
-        });
-
-        const content = repoConnection.get(id);
-        if (!content) {
-            logger.info(`Content ${id} was not found in draft branch of repo ${repo}!`);
-            return;
-        }
-
-        if (repo.endsWith(content.originProject)) {
-            logger.info(`Content ${id} originated in this repo (${repo})`);
-            return;
-        }
-
-        if (isLocalized(content)) {
-            logger.info(`Content ${id} was already processed in this repo (${repo})`);
-            return;
-        }
-
-        const result = repoConnection.modify({
-            key: id,
-            editor: (content) => {
-                setIsLocalized(content);
-
-                if (!hasValidCustomPath(content)) {
-                    return content;
-                }
-
-                logger.info(`Content ${id} has a customPath set, modifying...`);
-                const { customPath } = content.data;
-                const newPath = buildLocalePath(customPath, locale);
-
-                if (newPath === customPath) {
-                    logger.info(`Localized custom path was already set`);
-                    return content;
-                }
-
-                content.data.customPath = newPath;
-
-                return content;
-            },
-        });
-
-        logger.info(`Modify result: ${JSON.stringify(result)}`);
-    });
-};
 
 // Publish/unpublish actions in the root layer should be propagated to non-localized content in child
 // layers, as XP does not do this automatically
@@ -103,16 +15,14 @@ const propagatePublishEventsToLayers = (event: EnonicEvent) => {
         return;
     }
 
-    removeDuplicates(event.data.nodes, (a, b) => a.id === b.id).forEach((node) => {
+    event.data.nodes.forEach((node) => {
         const { id, branch, repo } = node;
 
         if (branch !== 'master' || repo !== CONTENT_ROOT_REPO_ID) {
             return;
         }
 
-        const nonLocalizedContent = getContentFromAllLayers(id, branch, 'nonlocalized');
-
-        nonLocalizedContent.forEach(({ repoId, locale }) => {
+        getContentFromAllLayers(id, branch, 'nonlocalized').forEach(({ repoId, locale }) => {
             if (repoId === CONTENT_ROOT_REPO_ID) {
                 return;
             }
@@ -152,11 +62,11 @@ export const activateLayersEventListeners = () => {
         return;
     }
 
+    hasSetupListeners = true;
+
     eventLib.listener({
         type: '(node.pushed|node.deleted)',
         localOnly: false,
         callback: propagatePublishEventsToLayers,
     });
-
-    hasSetupListeners = true;
 };
--- src/main/resources/lib/localization/locale-utils.ts
+++ src/main/resources/lib/localization/locale-utils.ts
@@ -1,14 +1,12 @@
 import * as nodeLib from '/lib/xp/node';
 import { MultiRepoNodeQueryHit } from '/lib/xp/node';
 import * as contentLib from '/lib/xp/content';
-import { Content } from '/lib/xp/content';
+import { Content, BooleanFilter } from '/lib/xp/content';
 import { RepoBranch } from '../../types/common';
 import { getLayersData } from './layers-data';
 import { logger } from '../utils/logging';
 import { runInLocaleContext } from './locale-context';
 import { forceArray } from '../utils/nav-utils';
-
-export type NodeHitsLocaleBuckets = Record<string, string[]>;
 
 export const getLayersMultiConnection = (branch: RepoBranch) => {
     return nodeLib.multiRepoConnect({
@@ -16,19 +14,32 @@ export const getLayersMultiConnection = (branch: RepoBranch) => {
     });
 };
 
+const isNonLocalizedFilter = [
+    {
+        hasValue: {
+            field: 'inherit',
+            values: ['CONTENT'],
+        },
+    },
+];
+
+type LocalizationState = 'localized' | 'nonlocalized' | 'all';
+const localizationStateFilter: Record<LocalizationState, BooleanFilter['boolean']> = {
+    localized: { mustNot: isNonLocalizedFilter },
+    nonlocalized: { must: isNonLocalizedFilter },
+    all: {},
+};
+
 type ContentAndLayerData = {
     content: Content;
     locale: string;
     repoId: string;
 };
-
 export const getContentFromAllLayers = (
     contentId: string,
     branch: RepoBranch,
-    type: 'localized' | 'nonlocalized' | 'all'
+    type: LocalizationState
 ): ContentAndLayerData[] => {
-    const booleanFilterKey = type === 'localized' ? 'mustNot' : 'must';
-
     const localizedNodes = getLayersMultiConnection(branch).query({
         start: 0,
         count: 100,
@@ -36,16 +47,7 @@ export const getContentFromAllLayers = (
             ids: {
                 values: [contentId],
             },
-            boolean: {
-                [booleanFilterKey]: [
-                    {
-                        hasValue: {
-                            field: 'inherit',
-                            values: ['CONTENT'],
-                        },
-                    },
-                ],
-            },
+            boolean: localizationStateFilter[type],
         },
     }).hits;
 
@@ -86,6 +88,7 @@ export const buildLocalePath = (basePath: string, locale: string) => {
 export const isContentLocalized = (content: Content) =>
     !forceArray(content.inherit).includes('CONTENT');
 
+export type NodeHitsLocaleBuckets = Record<string, string[]>;
 export const sortMultiRepoNodeHitIdsToLocaleBuckets = (hits: readonly MultiRepoNodeQueryHit[]) => {
     return hits.reduce<NodeHitsLocaleBuckets>((acc, node) => {
         const { repoId, id } = node;
