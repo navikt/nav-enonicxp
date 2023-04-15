1a5252ab3e06a343d50c9ec2bd75961c98327313
Anders Bråthen Nomerstad <anders@nomerstad.com>
2023-04-15 14:16:13 +0200

    Nok et forsøk på å installere chuck norris!

--- .nais/deploy.sh
+++ .nais/deploy.sh
@@ -9,3 +9,24 @@ then
 fi
 
 echo "XP user: $XP_USER"
+
+if [[ -z $XP_PASSWORD ]]
+then
+  echo "XP_PASSWORD must be specified"
+  exit
+fi
+
+if [[ -z $XP_INSTALL_API ]]
+then
+  echo "XP_INSTALL_API must be specified"
+  exit
+fi
+
+echo "XP api: $XP_INSTALL_API"
+
+curl \
+  -v \
+  --cacert /etc/pki/tls/cacert.pem \
+  -k "$XP_INSTALL_API" \
+  --user "$XP_USER:$XP_PASSWORD" \
+  -F file="chucknorris-2.2.3.jar"
