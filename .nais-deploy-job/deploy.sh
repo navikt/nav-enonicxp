ace7198a5c18f91eb31583de02ad256ab032139b
Anders Bråthen Nomerstad <anders@nomerstad.com>
2023-04-17 13:54:16 +0200

    Setter app file name som parameter

--- .nais-deploy-job/deploy.sh
+++ .nais-deploy-job/deploy.sh
@@ -2,6 +2,16 @@
 
 echo "Running deploy script!"
 
+APP_FILE_NAME=$1
+
+if [[ -z $APP_FILE_NAME ]]
+then
+  echo "APP_FILE_NAME must be specified"
+  exit
+fi
+
+echo "APP_FILE_NAME: $APP_FILE_NAME"
+
 if [[ -z $XP_USER ]]
 then
   echo "XP_USER must be specified"
@@ -23,14 +33,6 @@ then
 fi
 
 echo "XP api: $XP_INSTALL_API"
-
-if [[ -z $APP_FILE_NAME ]]
-then
-  echo "APP_FILE_NAME must be specified"
-  exit
-fi
-
-echo "APP_FILE_NAME: $APP_FILE_NAME"
 
 curl \
   --cacert /etc/pki/tls/cacert.pem \
