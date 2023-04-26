c97457949507ef3fef224af2bfba0587e4a42984
Anders Bråthen Nomerstad <anders@nomerstad.com>
2023-04-26 15:41:11 +0200

    Oppdaterer referanser i alle layers ved migrering

--- src/main/resources/lib/localization/layers-migration/update-content-references.ts
+++ src/main/resources/lib/localization/layers-migration/update-content-references.ts
@@ -9,6 +9,7 @@ import { getLayersData } from '../layers-data';
 import { RepoBranch } from '../../../types/common';
 import { modifyContentNode } from './modify-content-node';
 import { forceArray } from '../../utils/array-utils';
+import { isContentLocalized } from '../locale-utils';
 
 const updateReferenceFromNode = ({
     contentNodeToUpdate,
@@ -78,32 +79,39 @@ const updateReferenceFromNode = ({
     pushResult.success.forEach((id) => `Pushing ${id} to master succeeded`);
 };
 
-export const updateContentReferences = ({
-    sourceId,
-    targetId,
-    sourceLocale,
-}: ContentMigrationParams) => {
-    const references = runInLocaleContext({ locale: sourceLocale }, () =>
+const updateContentReferencesInLocaleLayer = (
+    { sourceId, targetId, sourceLocale }: ContentMigrationParams,
+    localeToUpdate: string
+) => {
+    const sourceRepoId = getLayersData().localeToRepoIdMap[localeToUpdate];
+    if (!sourceRepoId) {
+        logger.error(`Invalid locale specified ${localeToUpdate}`);
+        return;
+    }
+
+    const references = runInLocaleContext({ locale: localeToUpdate }, () =>
         findReferences({ id: sourceId, branch: 'master', withDeepSearch: false })
     );
     if (!references) {
-        logger.error(`References search failed for ${sourceId} in locale ${sourceLocale}`);
+        logger.error(`References search failed for ${sourceId} in locale ${localeToUpdate}`);
         return false;
     }
 
     logger.info(
         `Found ${
             references.length
-        } references to ${sourceId} in locale ${sourceLocale}:\n${references
+        } references to ${sourceId} in locale ${localeToUpdate}:\n${references
             .map((ref) => ref._path)
             .join('\n')}`
     );
 
-    const sourceRepoId = getLayersData().localeToRepoIdMap[sourceLocale];
-
     references.forEach((refContent) => {
+        if (!isContentLocalized(refContent)) {
+            return;
+        }
+
         const { _id } = refContent;
-        if (_id === sourceId) {
+        if (_id === sourceId && localeToUpdate === sourceLocale) {
             return;
         }
 
@@ -143,4 +151,19 @@ export const updateContentReferences = ({
     });
 
     return true;
+};
+
+export const updateContentReferences = (params: ContentMigrationParams) => {
+    const { locales } = getLayersData();
+
+    let isSuccess = true;
+
+    locales.forEach((locale) => {
+        const result = updateContentReferencesInLocaleLayer(params, locale);
+        if (!result) {
+            isSuccess = false;
+        }
+    });
+
+    return isSuccess;
 };
