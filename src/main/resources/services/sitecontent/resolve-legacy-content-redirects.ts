e8069cf2040cc7408ed23d2176aa9e44c050fa57
Terje Karlsen <terjeofnorway@gmail.com>
2023-05-03 14:23:52 +0200

    Forberer match av gammelt mot nytt kontor

--- src/main/resources/services/sitecontent/resolve-legacy-content-redirects.ts
+++ src/main/resources/services/sitecontent/resolve-legacy-content-redirects.ts
@@ -1,3 +1,6 @@
+import { OfficeBranch } from 'site/content-types/office-branch/office-branch';
+import { Content } from '/lib/xp/content';
+
 import { transformToRedirectResponse } from './resolve-redirects';
 import * as contentLib from '/lib/xp/content';
 
@@ -9,10 +12,33 @@ export const resolveLegacyContentRedirects = (content: contentLib.Content) => {
     }
 
     if (content.type === 'no.nav.navno:office-information') {
-        const { _name } = content;
+        const { enhetNr } = content.data.enhet;
+        const oldName = content._name;
+
+        const foundOfficeContent = contentLib.query({
+            start: 0,
+            count: 1,
+            contentTypes: ['no.nav.navno:office-branch'],
+            filters: {
+                boolean: {
+                    must: {
+                        hasValue: {
+                            field: 'data.enhetNr',
+                            values: [enhetNr],
+                        },
+                    },
+                },
+            },
+        });
+
+        // Try and use the new office branch name, but fall back to the old name if it
+        // will return a 404 after redirect.
+        const officeName =
+            foundOfficeContent.hits.length > 0 ? foundOfficeContent.hits[0]._name : oldName;
+
         return transformToRedirectResponse({
             content,
-            target: `/kontor/${_name}`,
+            target: `/kontor/${officeName}`,
             type: 'internal',
             isPermanent: true,
         });
