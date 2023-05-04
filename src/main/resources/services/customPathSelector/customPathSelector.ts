034faab54d8cf5a79cdd18b36f3e94db582656ab
Anders Bråthen Nomerstad <anders@nomerstad.com>
2023-05-04 10:55:06 +0200

    Validerer mellomsteg path

--- src/main/resources/services/customPathSelector/customPathSelector.ts
+++ src/main/resources/services/customPathSelector/customPathSelector.ts
@@ -11,6 +11,8 @@ import { customSelectorErrorIcon, customSelectorWarningIcon } from '../custom-se
 import { runInContext } from '../../lib/context/run-in-context';
 import { forceArray } from '../../lib/utils/array-utils';
 
+type SpecialUrlType = 'formIntermediateStep';
+
 // Returns an error message to the editor with an intentionally invalid id (customPath id must start with '/')
 const generateErrorHit = (displayName: string, description: string) => ({
     id: `error-${Date.now()}`,
@@ -41,17 +43,15 @@ const getResult = ({
 }: {
     query?: string;
     ids?: string | string[];
-}): XP.CustomSelectorServiceResponseHit[] => {
+}): XP.CustomSelectorServiceResponseHit => {
     const currentSelection = forceArray(ids)[0];
     const suggestedPath = query || currentSelection;
 
     if (!isValidCustomPath(suggestedPath)) {
-        return [
-            generateErrorHit(
-                `Feil: "${suggestedPath}" er ikke en gyldig kort-url`,
-                'Kort-url må starte med "/" og kan inneholde tall, bokstaver (a-z) og bindestrek'
-            ),
-        ];
+        return generateErrorHit(
+            `Feil: "${suggestedPath}" er ikke en gyldig kort-url`,
+            'Kort-url må starte med "/" og kan inneholde tall, bokstaver (a-z) og bindestrek'
+        );
     }
 
     if (suggestedPath !== currentSelection) {
@@ -59,12 +59,10 @@ const getResult = ({
         if (contentWithCustomPath) {
             const currentContent = portalLib.getContent();
             if (currentContent._id !== contentWithCustomPath._id) {
-                return [
-                    generateErrorHit(
-                        `Feil: "${suggestedPath}" er allerede i bruk som kort-url`,
-                        `"${contentWithCustomPath._path}" bruker allerede denne kort-url'en`
-                    ),
-                ];
+                return generateErrorHit(
+                    `Feil: "${suggestedPath}" er allerede i bruk som kort-url`,
+                    `"${contentWithCustomPath._path}" bruker allerede denne kort-url'en`
+                );
             }
         }
     }
@@ -73,57 +71,71 @@ const getResult = ({
         contentLib.get({ key: `${NAVNO_ROOT_PATH}${suggestedPath}` })
     );
     if (contentWithInternalPath && contentWithInternalPath.type !== 'portal:site') {
-        return [
-            generateErrorHit(
-                `Feil: "${suggestedPath}" er allerede i bruk som vanlig url`,
-                `"${contentWithInternalPath.displayName}" har denne url'en`
-            ),
-        ];
+        return generateErrorHit(
+            `Feil: "${suggestedPath}" er allerede i bruk som vanlig url`,
+            `"${contentWithInternalPath.displayName}" har denne url'en`
+        );
     }
 
     const ingressIsOurs = verifyIngressOwner(suggestedPath);
     if (!ingressIsOurs) {
-        return [
-            generateErrorHit(
-                `Feil: "${suggestedPath}" kan tilhøre en annen app på nav.no`,
-                "Det krever en teknisk endring for å bruke denne url'en, kontakt Team personbruker"
-            ),
-        ];
+        return generateErrorHit(
+            `Feil: "${suggestedPath}" kan tilhøre en annen app på nav.no`,
+            "Det krever en teknisk endring for å bruke denne url'en, kontakt Team personbruker"
+        );
     }
 
     const redirectContent = runInContext({ branch: 'master' }, () =>
         contentLib.get({ key: `${REDIRECTS_ROOT_PATH}${suggestedPath}` })
     );
     if (redirectContent && redirectContent.type !== 'base:folder') {
-        return [
-            {
-                id: suggestedPath,
-                displayName: suggestedPath,
-                description: `Advarsel: ${suggestedPath} er i bruk som redirect url - redirect vil overstyres av kort-url`,
-                icon: customSelectorWarningIcon,
-            },
-        ];
-    }
-
-    return [
-        {
+        return {
             id: suggestedPath,
             displayName: suggestedPath,
-            description: `Denne siden vil kunne nåes på nav.no${suggestedPath}`,
-        },
-    ];
+            description: `Advarsel: ${suggestedPath} er i bruk som redirect url - redirect vil overstyres av kort-url`,
+            icon: customSelectorWarningIcon,
+        };
+    }
+
+    return {
+        id: suggestedPath,
+        displayName: suggestedPath,
+        description: `Denne siden vil kunne nåes på nav.no${suggestedPath}`,
+    };
+};
+
+const validateFormIntermediateStepResult = (result: XP.CustomSelectorServiceResponseHit) => {
+    const content = portalLib.getContent();
+    // const audience = content.data.audience;
+};
+
+const validateResult = (
+    result: XP.CustomSelectorServiceResponseHit,
+    type?: SpecialUrlType
+): XP.CustomSelectorServiceResponse | null => {
+    if (type === 'formIntermediateStep') {
+        return validateFormIntermediateStepResult(result);
+    }
+
+    return null;
 };
 
 export const get = (req: XP.CustomSelectorServiceRequest): XP.CustomSelectorServiceResponse => {
-    const { query, ids } = req.params;
+    const { query, ids, type } = req.params;
+
     const result = getResult({ query, ids });
+
+    const errorResponse = validateResult(result, type as SpecialUrlType);
+    if (errorResponse) {
+        return errorResponse;
+    }
 
     return {
         status: 200,
         body: {
-            total: result.length,
-            count: result.length,
-            hits: result,
+            total: 1,
+            count: 1,
+            hits: [result],
         },
         contentType: 'application/json',
     };
