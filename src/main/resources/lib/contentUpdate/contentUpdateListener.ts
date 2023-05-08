e910633d2a79782cce84d08c98bfa9b26dc553c8
Terje Karlsen <terjeofnorway@gmail.com>
2023-05-08 15:04:33 +0200

    Forbedrer feilhåndtering ved henting fra qbrick

--- src/main/resources/lib/contentUpdate/contentUpdateListener.ts
+++ src/main/resources/lib/contentUpdate/contentUpdateListener.ts
@@ -1,16 +1,17 @@
 import * as eventLib from '/lib/xp/event';
+import * as contentLib from '/lib/xp/content';
 import { Content } from '/lib/xp/content';
 import httpClient from '/lib/http-client';
-import * as contentLib from '/lib/xp/content';
 import { findNestedKey } from '../utils/object-utils';
 import { runInContext } from '../context/run-in-context';
 
-let didActivateListener = false;
+let hasContentUpdateListener = false;
 type UpdateVideoContentParams = {
     content: Content<'no.nav.navno:video'>;
     duration: number;
     imageAsset: Content<'media:image'>;
 };
+
 const updateVideoContent = ({ content, duration, imageAsset }: UpdateVideoContentParams) => {
     contentLib.modify({
         key: content._id,
@@ -61,7 +62,7 @@ const fetchMetaDataFromQbrick = (accountId: number, mediaId: string) => {
         const data = JSON.parse(response.body);
         const duration = findNestedKey(data, 'duration');
         const links = findNestedKey(data, 'links');
-        const imageURl = links[0].href;
+        const imageURl = links.length > 0 ? links[0].href : null;
         return { duration, imageURl };
     } catch (e) {
         log.error('Failed to fetch video data from Qbrick', e);
@@ -69,16 +70,18 @@ const fetchMetaDataFromQbrick = (accountId: number, mediaId: string) => {
     }
 };
 
-const updateVideoContentWithMedaData = (content: Content<'no.nav.navno:video'>) => {
+const updateVideoContentWithMetaData = (content: Content<'no.nav.navno:video'>) => {
     const { accountId, mediaId } = content.data;
 
+    // Don't override any posters or durations already set or set manually
+    // by the editor.
     if (content.data.poster || content.data.duration) {
         return;
     }
 
     const qbrickMetadata = fetchMetaDataFromQbrick(accountId, mediaId);
 
-    if (!qbrickMetadata) {
+    if (!qbrickMetadata || !qbrickMetadata.duration || !qbrickMetadata.imageURl) {
         return;
     }
 
@@ -94,16 +97,16 @@ const handleEvent = (event: eventLib.EnonicEvent) => {
     const content = contentLib.get({ key: id });
 
     if (content && content.type === 'no.nav.navno:video') {
-        updateVideoContentWithMedaData(content);
+        updateVideoContentWithMetaData(content);
     }
 };
 
-export const activateMacroUpdateListener = () => {
-    if (didActivateListener) {
+export const activateContentUpdateListener = () => {
+    if (hasContentUpdateListener) {
         return;
     }
 
-    didActivateListener = true;
+    hasContentUpdateListener = true;
 
     eventLib.listener({
         type: 'node.updated',
