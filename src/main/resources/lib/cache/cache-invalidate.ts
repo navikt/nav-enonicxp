0077f1c1d68b5eb920784b4758f7657c9eab910f
Anders Bråthen Nomerstad <anders@nomerstad.com>
2023-07-07 15:36:08 +0200

    Søker alltid i alle layers etter referanser for cache-invalidering

--- src/main/resources/lib/cache/cache-invalidate.ts
+++ src/main/resources/lib/cache/cache-invalidate.ts
@@ -16,7 +16,6 @@ import { getLayersData } from '../localization/layers-data';
 import { isContentLocalized } from '../localization/locale-utils';
 import { removeDuplicates } from '../utils/array-utils';
 import { getPublicPath } from '../paths/public-path';
-import { CONTENT_LOCALE_DEFAULT } from '../constants';
 
 export const CACHE_INVALIDATE_EVENT_NAME = 'invalidate-cache';
 
@@ -24,46 +23,24 @@ const REFERENCE_SEARCH_TIMEOUT_MS = 10000;
 
 const getPaths = (contents: Content[], locale: string) =>
     contents.reduce<string[]>((acc, content) => {
-        if (!isPublicRenderedType(content)) {
-            return acc;
-        }
-
-        acc.push(getPublicPath(content, locale));
-
-        // Always include the path for the default locale as well, to be on the safe side :)
-        if (locale !== CONTENT_LOCALE_DEFAULT) {
-            acc.push(getPublicPath(content, CONTENT_LOCALE_DEFAULT));
+        if (isPublicRenderedType(content)) {
+            acc.push(getPublicPath(content, locale));
         }
 
         return acc;
     }, []);
 
-const resolveReferencePaths = (id: string, eventType: string, locale: string) => {
+const resolveReferencePaths = (id: string, eventType: string) => {
     // If the content was deleted, we must check in the draft branch for references
     const branch = eventType === 'node.deleted' ? 'draft' : 'master';
 
-    const { defaultLocale, locales } = getLayersData();
+    const { locales } = getLayersData();
 
     const deadline = Date.now() + REFERENCE_SEARCH_TIMEOUT_MS;
 
-    const contentToInvalidate = findReferences({ id, branch, deadline });
-    if (!contentToInvalidate) {
-        return null;
-    }
-
-    const pathsToInvalidate = getPaths(contentToInvalidate, locale);
-
-    // If the locale is not the default, we're done. Otherwise, we need to check if any of the
-    // references found are also referenced in the child layers
-    if (locale !== defaultLocale) {
-        return removeDuplicates(pathsToInvalidate);
-    }
+    const pathsToInvalidate: string[] = [];
 
     const success = locales.every((locale) => {
-        if (locale === defaultLocale) {
-            return true;
-        }
-
         const references = runInLocaleContext({ locale }, () =>
             findReferences({ id, branch, deadline })
         );
@@ -92,7 +69,7 @@ const resolveReferencesAndInvalidateFrontend = ({
 
     runInLocaleContext({ branch: 'master', locale }, () => {
         const eventId = generateCacheEventId(node, timestamp);
-        const pathsToInvalidate = resolveReferencePaths(node.id, eventType, locale);
+        const pathsToInvalidate = resolveReferencePaths(node.id, eventType);
 
         if (!pathsToInvalidate) {
             logger.warning(`Resolving paths for references failed for eventId ${eventId}`);
