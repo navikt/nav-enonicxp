34b5f4abde5d7de68933d582b0648be8746ab5a6
Terje Karlsen <terjeofnorway@gmail.com>
2023-07-19 12:04:06 +0200

    Legger til tjeneste for sjekk av hvor video-elementer er brukt i makroer

---
+++ src/main/resources/services/video/video.ts
@@ -0,0 +1,60 @@
+import * as contentLib from '/lib/xp/content';
+import { Content } from '/lib/xp/content';
+
+import { getServiceRequestSubPath, transformUsageHit } from '../service-utils';
+
+import { logger } from '../../lib/utils/logging';
+
+type UsageCheckParams = {
+    videoId: string;
+};
+
+export const getVideoUsage = (content: Content<'no.nav.navno:video'>) => {
+    const contentWithUsage = contentLib.query({
+        start: 0,
+        count: 1000,
+        query: `components.part.config.no-nav-navno.html-area.html LIKE '*targetContent="${content._id}"*' OR data.text LIKE '*targetContent="${content._id}"*'`,
+    }).hits;
+
+    return [...contentWithUsage];
+};
+
+const usageCheckHandler = (req: XP.Request) => {
+    const { videoId } = req.params as UsageCheckParams;
+
+    const detailsContent = contentLib.get({ key: videoId });
+    if (!detailsContent || detailsContent.type !== 'no.nav.navno:video') {
+        logger.warning(
+            `Product details usage check for id ${videoId} failed - content does not exist`
+        );
+        return {
+            status: 404,
+            contentType: 'application/json',
+            body: {
+                usage: [],
+            },
+        };
+    }
+
+    const usageHits = getVideoUsage(detailsContent).map(transformUsageHit);
+
+    return {
+        status: 200,
+        contentType: 'application/json',
+        body: {
+            usage: usageHits,
+        },
+    };
+};
+
+export const get = (req: XP.Request) => {
+    const subPath = getServiceRequestSubPath(req);
+
+    if (subPath === 'usage') {
+        return usageCheckHandler(req);
+    }
+
+    return {
+        status: 201,
+    };
+};
