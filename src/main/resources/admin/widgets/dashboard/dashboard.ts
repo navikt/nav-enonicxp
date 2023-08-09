0ccf41082cc9f14d928b7a540956a8e34e0fca89
perolavm <per.olav.mariussen@nav.no>
2023-08-09 12:55:20 +0200

    Satt opp basic mvc for widget

--- src/main/resources/admin/widgets/dashboard/dashboard.ts
+++ src/main/resources/admin/widgets/dashboard/dashboard.ts
@@ -1,6 +1,13 @@
+import * as contentLib from '/lib/xp/content';
+import * as portalLib from '/lib/xp/portal';
+import thymeleafLib from '/lib/thymeleaf';
+
+const view = resolve('./dashboard.html');
+const text = "Skal erstattes med en contentLib.get()"
+const model = { text };
 const dashboardInfo = () => {
     return {
-        body: '<widget>Informasjon til redaktører</widget>',
+        body: thymeleafLib.render(view, model),
         contentType: 'text/html',
     };
 };
