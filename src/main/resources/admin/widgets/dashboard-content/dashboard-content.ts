46827dd1a0ba9c0dc408c038c3b74a073f12a907
Skifjeld <eivind.skifjeld@nav.no>
2023-09-28 12:06:49 +0200

    Laget to tabeller. En for endret og en for publisert

--- src/main/resources/admin/widgets/dashboard-content/dashboard-content.ts
+++ src/main/resources/admin/widgets/dashboard-content/dashboard-content.ts
@@ -3,30 +3,70 @@ import thymeleafLib from '/lib/thymeleaf';
 import { runInContext } from "../../../lib/context/run-in-context";
 import * as authLib from '/lib/xp/auth';
 import * as contentLib from '/lib/xp/content';
-import { logger } from '../../../lib/utils/logging'; 
+import { logger } from '../../../lib/utils/logging';
 
+type ContentInfo={ 
+    displayName: string,
+    modifiedTime: string,
+    status: string, 
+    url: string
+}
+
+const view = resolve('./dashboard-content.html');
 
 const getModifiedContentFromUser = () => {
 
-    const user = authLib.getUser()?.key;  
-    
-    logger.info(JSON.stringify(user));   
-  
+    const user = authLib.getUser()?.key;    
     const results = contentLib.query({
         start: 0,
-        count: 2,
+        count: 20,
         sort: "modifiedTime DESC",
         query: `modifier = "${user}"`
     }).hits.map((hit) => { 
+        let status="Ny";
+        if (hit.publish?.first) {
+            if (hit.publish?.from) {
+                if (hit.modifiedTime > hit.publish.from) {
+                    status="Endret";
+                }
+                else {
+                    status="Publisert";
+                }
+            }                    
+            else {
+                if (hit.modifiedTime > hit.publish.first) {
+                    status="Endret";
+                }
+                else {
+                    status="Avpublisert";
+                }
+            }     
+        }               
         return { 
             displayName: hit.displayName, 
             modifiedTime: hit.modifiedTime, 
-            published: !!hit.publish?.from, 
+            status, 
             url: `/admin/tool/com.enonic.app.contentstudio/main/default/edit/${hit._id}`
         }
     });
+    
+    const published:ContentInfo[] = [];
+    const modified = results.map(result => {
+        if (result.status === "Publisert") {
+            published.push(result);
+        }
+        else {
+            return result;
+        }
+    })
 
-    logger.info(JSON.stringify(results));      
+    logger.info(JSON.stringify(published));
+    logger.info(JSON.stringify(modified)); 
+    
+    return {
+        body: thymeleafLib.render(view,{modified, published}),
+        contentType: 'text/html',
+    };       
 }
 
 exports.get = getModifiedContentFromUser;
@@ -36,6 +76,7 @@ exports.get = getModifiedContentFromUser;
 // Hente innlogget bruker user via authLib
 // modifier === user,  --> Hente data fra content
 // hente ut modifiedTime fra innholdselementer
+// Ny ..> ikke pubblished , 
 // Sortere content fra yngst til eldst -> Siste to uker?
 // lenker til innholdselementer og vise publiseringsstatus og tid
 // ikke behov for ny innholdstype
