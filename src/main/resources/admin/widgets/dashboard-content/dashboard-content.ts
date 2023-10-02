0496be26ce464f6f74b64b6fe83ee55dda60c3f5
Skifjeld <eivind.skifjeld@nav.no>
2023-10-02 18:23:53 +0200

    Sjekk på om version_key er den samme i master og draft

--- src/main/resources/admin/widgets/dashboard-content/dashboard-content.ts
+++ src/main/resources/admin/widgets/dashboard-content/dashboard-content.ts
@@ -4,6 +4,27 @@ import { runInContext } from "../../../lib/context/run-in-context";
 import * as authLib from '/lib/xp/auth';
 import * as contentLib from '/lib/xp/content';
 import { logger } from '../../../lib/utils/logging';
+import * as nodeLib from '/lib/xp/node';
+import { Source } from '/lib/xp/node';
+import { ADMIN_PRINCIPAL, CONTENT_REPO_PREFIX, SUPER_USER } from '../../../lib/constants';
+
+const asAdminParams: Pick<Source, 'user' | 'principals'> = {
+    user: {
+        login: SUPER_USER,
+    },
+    principals: [ADMIN_PRINCIPAL],
+};
+
+type Params = Omit<Source, 'user' | 'principals'> & { asAdmin?: boolean };
+
+const getRepoConnection = ({ repoId, branch, asAdmin }: Params) =>
+
+nodeLib.connect({
+repoId,
+branch,
+...(asAdmin && asAdminParams),
+}
+);
 
 type ContentInfo={ 
     displayName: string,
@@ -19,34 +40,36 @@ const getModifiedContentFromUser = () => {
     const user = authLib.getUser()?.key;    
     const results = contentLib.query({
         start: 0,
-        count: 20,
+        count: 10,
         sort: "modifiedTime DESC",
         query: `modifier = "${user}"`
     }).hits.map((hit) => { 
+
+        const draftContent = getRepoConnection({ branch: 'draft', repoId: 'com.enonic.cms.default' }).get(hit._id);
+        const masterContent = getRepoConnection({ branch: 'master', repoId: 'com.enonic.cms.default' }).get(hit._id); 
+
+        let convertModifiedTime = hit.modifiedTime; 
+        convertModifiedTime = convertModifiedTime.substring(0,10);
+
         let status="Ny";
+        
         if (hit.publish?.first) {
             if (hit.publish?.from) {
-                if (hit.modifiedTime > hit.publish.from) {
-                    status="Endret";
-                }
-                else {
-                    status="Publisert";
-                }
-            }                    
-            else {
-                if (hit.modifiedTime > hit.publish.first) {
-                    status="Endret";
-                }
-                else {
-                    status="Avpublisert";
-                }
+                    if (draftContent?._versionKey === masterContent?._versionKey ) { 
+                        status="Publisert" 
+                    }
+                    else {
+                        status="Endret";
+                    }      
+                }                    
+            else { status="Avpublisert";          
             }     
-        }               
+        }                     
         return { 
             displayName: hit.displayName, 
-            modifiedTime: hit.modifiedTime, 
+            modifiedTime: convertModifiedTime,
             status, 
-            url: `/admin/tool/com.enonic.app.contentstudio/main/default/edit/${hit._id}`
+            url: `/admin/tool/com.enonic.app.contentstudio/main/default/edit/target="_blank".${hit._id}`
         }
     });
     
@@ -59,9 +82,6 @@ const getModifiedContentFromUser = () => {
             return result;
         }
     })
-
-    logger.info(JSON.stringify(published));
-    logger.info(JSON.stringify(modified)); 
     
     return {
         body: thymeleafLib.render(view,{modified, published}),
@@ -70,24 +90,3 @@ const getModifiedContentFromUser = () => {
 }
 
 exports.get = getModifiedContentFromUser;
-
-
-
-// Hente innlogget bruker user via authLib
-// modifier === user,  --> Hente data fra content
-// hente ut modifiedTime fra innholdselementer
-// Ny ..> ikke pubblished , 
-// Sortere content fra yngst til eldst -> Siste to uker?
-// lenker til innholdselementer og vise publiseringsstatus og tid
-// ikke behov for ny innholdstype
-// Vises dette på dashboardet
-// Hva med plassering på dashboardet hvis det er en melding til redaktørene i tillegg?
-
-
-
-
-
-
-
-
-
