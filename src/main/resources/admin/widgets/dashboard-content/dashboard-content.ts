9285d50571d7d9df7144de98e0b1bcd911493617
Skifjeld <eivind.skifjeld@nav.no>
2023-10-05 09:12:56 +0200

    Slått sammen branches dashboard-widget

--- src/main/resources/admin/widgets/dashboard-content/dashboard-content.ts
+++ src/main/resources/admin/widgets/dashboard-content/dashboard-content.ts
@@ -1,6 +1,5 @@
-
 import thymeleafLib from '/lib/thymeleaf';
-import { runInContext } from "../../../lib/context/run-in-context";
+import { runInContext } from '../../../lib/context/run-in-context';
 import * as authLib from '/lib/xp/auth';
 import * as contentLib from '/lib/xp/content';
 import { logger } from '../../../lib/utils/logging';
@@ -18,75 +17,76 @@ const asAdminParams: Pick<Source, 'user' | 'principals'> = {
 type Params = Omit<Source, 'user' | 'principals'> & { asAdmin?: boolean };
 
 const getRepoConnection = ({ repoId, branch, asAdmin }: Params) =>
+    nodeLib.connect({
+        repoId,
+        branch,
+        ...(asAdmin && asAdminParams),
+    });
 
-nodeLib.connect({
-repoId,
-branch,
-...(asAdmin && asAdminParams),
-}
-);
-
-type ContentInfo={ 
-    displayName: string,
-    modifiedTime: string,
-    status: string, 
-    url: string
-}
+type ContentInfo = {
+    displayName: string;
+    modifiedTime: string;
+    status: string;
+    url: string;
+};
 
 const view = resolve('./dashboard-content.html');
 
 const getModifiedContentFromUser = () => {
+    const user = authLib.getUser()?.key;
+    const results = contentLib
+        .query({
+            start: 0,
+            count: 10,
+            sort: 'modifiedTime DESC',
+            query: `modifier = "${user}"`,
+        })
+        .hits.map((hit) => {
+            const draftContent = getRepoConnection({
+                branch: 'draft',
+                repoId: 'com.enonic.cms.default',
+            }).get(hit._id);
+            const masterContent = getRepoConnection({
+                branch: 'master',
+                repoId: 'com.enonic.cms.default',
+            }).get(hit._id);
 
-    const user = authLib.getUser()?.key;    
-    const results = contentLib.query({
-        start: 0,
-        count: 10,
-        sort: "modifiedTime DESC",
-        query: `modifier = "${user}"`
-    }).hits.map((hit) => { 
+            const convertModifiedTime = hit.modifiedTime?.substring(0, 10);
 
-        const draftContent = getRepoConnection({ branch: 'draft', repoId: 'com.enonic.cms.default' }).get(hit._id);
-        const masterContent = getRepoConnection({ branch: 'master', repoId: 'com.enonic.cms.default' }).get(hit._id); 
+            let status = 'Ny';
 
-        let convertModifiedTime = hit.modifiedTime; 
-        convertModifiedTime = convertModifiedTime.substring(0,10);
-
-        let status="Ny";
-        
-        if (hit.publish?.first) {
-            if (hit.publish?.from) {
-                    if (draftContent?._versionKey === masterContent?._versionKey ) { 
-                        status="Publisert" 
+            if (hit.publish?.first) {
+                if (hit.publish?.from) {
+                    if (draftContent?._versionKey === masterContent?._versionKey) {
+                        status = 'Publisert';
+                    } else {
+                        status = 'Endret';
                     }
-                    else {
-                        status="Endret";
-                    }      
-                }                    
-            else { status="Avpublisert";          
-            }     
-        }                     
-        return { 
-            displayName: hit.displayName, 
-            modifiedTime: convertModifiedTime,
-            status, 
-            url: `/admin/tool/com.enonic.app.contentstudio/main/default/edit/${hit._id}`
-        }
-    });
-    
-    const published:ContentInfo[] = [];
-    const modified = results.map(result => {
-        if (result.status === "Publisert") {
+                } else {
+                    status = 'Avpublisert';
+                }
+            }
+            return {
+                displayName: hit.displayName,
+                modifiedTime: convertModifiedTime,
+                status,
+                url: `/admin/tool/com.enonic.app.contentstudio/main/default/edit/${hit._id}`,
+            };
+        });
+
+    const published: ContentInfo[] = [];
+    const modified = results.map((result) => {
+        if (result.status === 'Publisert') {
             published.push(result);
-        }
-        else {
+        } else {
             return result;
         }
-    })
-    
+    });
+
     return {
-        body: thymeleafLib.render(view,{modified, published}),
+        body: thymeleafLib.render(view, { modified, published }),
         contentType: 'text/html',
-    };       
-}
+    };
+};
 
 exports.get = getModifiedContentFromUser;
--- src/main/resources/admin/widgets/dashboard/dashboard.ts
+++ src/main/resources/admin/widgets/dashboard/dashboard.ts
@@ -1,18 +1,18 @@
 import * as contentLib from '/lib/xp/content';
 import thymeleafLib from '/lib/thymeleaf';
-import { runInContext } from "../../../lib/context/run-in-context";
+import { runInContext } from '../../../lib/context/run-in-context';
 
 const announcementId = '/www.nav.no/admin/announcement';
 const view = resolve('./dashboard.html');
 
 const dashboardInfo = () => {
     const content = runInContext({ branch: 'master' }, () =>
-        contentLib.get<any>({key: announcementId})
+        contentLib.get<any>({ key: announcementId })
     );
 
     if (content && content.type === 'no.nav.navno:announcement-to-editors') {
         const { displayName, text } = content.data;
-        const model = { displayName, text};
+        const model = { displayName, text };
 
         return {
             body: thymeleafLib.render(view, model),
@@ -22,7 +22,7 @@ const dashboardInfo = () => {
     return {
         body: null,
         contentType: 'text/html; charset=UTF-8',
-    }
+    };
 };
 
 exports.get = dashboardInfo;
