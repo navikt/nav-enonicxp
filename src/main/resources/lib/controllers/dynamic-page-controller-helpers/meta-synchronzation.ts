d3620774caf5f7dd05ea2ebbfb499c7ddac2770b
Terje Karlsen <terjeofnorway@gmail.com>
2023-11-15 14:48:18 +0100

    Henter ut baseContent som skal synkroniseres mot

--- src/main/resources/lib/controllers/dynamic-page-controller-helpers/meta-synchronzation.ts
+++ src/main/resources/lib/controllers/dynamic-page-controller-helpers/meta-synchronzation.ts
@@ -1,4 +1,12 @@
-import * as contentLib from '/lib/xp/content';
+import * as portalLib from '/lib/xp/portal';
+import { getRepoConnection } from '../../utils/repo-utils';
+import { CONTENT_ROOT_REPO_ID } from '../../constants';
+import { logger } from '../../utils/logging';
+import { Content } from '/lib/xp/content';
+import { NodeContent } from '/lib/xp/node';
+import { getLayersData } from '../../localization/layers-data';
+
+type DynamicPageContent = NodeContent<Content>;
 
 const metaDataToCopy = [
     'audience',
@@ -17,8 +25,22 @@ const metaDataToCopy = [
 ];
 
 export const synchronizeMetaDataToLayers = (req: XP.Request) => {
-    // 1. Get the meta data to copy
-    // 2. Loop each layer and check for divergent data
-    // 3. Insert meta data into content
-    log.info('synchronizeMetaDataToLayers');
+    // 1. Check if content is on default layer.
+    // 2. If so, propagate meta to all layers
+    // 2. If not, get the content from default layer, then copy to this one.
+    // 5.
+
+    const { repoIdToLocaleMap } = getLayersData();
+
+    const content = portalLib.getContent();
+    if (!content) {
+        logger.error(`Could not get contextual content from request path - ${req.rawPath}`);
+        return;
+    }
+
+    log.info(JSON.stringify(content));
+
+    const defaultRepo = getRepoConnection({ repoId: CONTENT_ROOT_REPO_ID, branch: 'draft' });
+
+    //const defaultLayerContent = defaultRepo.get<DynamicPageContent>({ key: content._id });
 };
