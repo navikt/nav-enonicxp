e5b50f4cbfd94ad20baad015ddd1c1427a1772af
Anders Bråthen Nomerstad <anders@nomerstad.com>
2023-11-16 13:51:52 +0100

    Legger til manglende metatags for eksternt søk

--- src/main/resources/lib/search/external/document-builder/field-resolvers/metatags.ts
+++ src/main/resources/lib/search/external/document-builder/field-resolvers/metatags.ts
@@ -8,17 +8,18 @@ export type SearchDocumentMetatag =
     | 'skjema'
     | 'nyhet'
     | 'pressemelding'
-    | 'nav-og-samfunn' // legg til
+    | 'nav-og-samfunn'
     | 'analyse'
     | 'statistikk'
-    | 'presse'; // legg til
+    | 'presse';
 
 const isKontor = (content: ContentNode) => content.type === 'no.nav.navno:office-branch';
 
 const isSkjema = (content: ContentNode) => content.type === 'no.nav.navno:form-details';
 
 const isNyhet = (content: ContentNode) =>
-    content.type === 'no.nav.navno:main-article' && content.data.contentType === 'news';
+    (content.type === 'no.nav.navno:main-article' && content.data.contentType === 'news') ||
+    content.type === 'no.nav.navno:current-topic-page';
 
 const isPressemelding = (content: ContentNode) =>
     content.type === 'no.nav.navno:main-article' && content.data.contentType === 'pressRelease';
@@ -29,6 +30,25 @@ const isStatistikk = (content: ContentNode) =>
 
 const isAnalyse = (content: ContentNode) =>
     content._path.startsWith('/content/www.nav.no/no/nav-og-samfunn/kunnskap');
+
+const isNavOgSamfunn = (content: ContentNode) =>
+    content._path.startsWith('/content/www.nav.no/no/nav-og-samfunn') &&
+    !content._path.startsWith('/content/www.nav.no/no/nav-og-samfunn/statistikk');
+
+const isPresse = (content: ContentNode) =>
+    isPressemelding(content) ||
+    (isNyhet(content) &&
+        (content._path.startsWith(
+            '/content/www.nav.no/no/person/innhold-til-person-forside/nyheter*'
+        ) ||
+            content._path.startsWith(
+                '/content/www.nav.no/no/nav-og-samfunn/kunnskap/analyser-fra-nav'
+            ) ||
+            !(
+                content._path.startsWith('/content/www.nav.no/no/person') ||
+                content._path.startsWith('/content/www.nav.no/no/bedrift') ||
+                content._path.startsWith('/content/www.nav.no/no/nav-og-samfunn')
+            )));
 
 export const getSearchDocumentMetatags = (content: ContentNode) => {
     const metaTags: SearchDocumentMetatag[] = [];
@@ -50,6 +70,12 @@ export const getSearchDocumentMetatags = (content: ContentNode) => {
     }
     if (isAnalyse(content)) {
         metaTags.push('analyse');
+    }
+    if (isNavOgSamfunn(content)) {
+        metaTags.push('nav-og-samfunn');
+    }
+    if (isPresse(content)) {
+        metaTags.push('presse');
     }
 
     return metaTags.length > 0 ? metaTags : undefined;
