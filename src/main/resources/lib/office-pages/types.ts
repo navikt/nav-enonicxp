ecb6d23ec06d66948086909df32550addbe8b33b
Anders Bråthen Nomerstad <anders@nomerstad.com>
2024-02-09 13:38:26 +0100

    Ingress for kontorsider

--- src/main/resources/lib/office-pages/types.ts
+++ src/main/resources/lib/office-pages/types.ts
@@ -3,10 +3,10 @@ import { ContentDescriptor } from '../../types/content-types/content-config';
 
 export type OfficeContent = Content<(typeof OFFICE_PAGE_TYPES)[number]>;
 
-const OFFICE_PAGE_TYPES: ReadonlyArray<ContentDescriptor> = [
+const OFFICE_PAGE_TYPES = [
     'no.nav.navno:office-branch',
     'no.nav.navno:office-information',
-] as const;
+] as const satisfies ReadonlyArray<ContentDescriptor>;
 
 export const isOfficeContent = (content: Content): content is OfficeContent =>
-    OFFICE_PAGE_TYPES.includes(content.type);
+    OFFICE_PAGE_TYPES.includes((content as OfficeContent).type);
--- src/main/resources/lib/search/external/document-builder/field-resolvers/office-ingress.ts
+++ src/main/resources/lib/search/external/document-builder/field-resolvers/office-ingress.ts
@@ -1,3 +1,45 @@
+import { Content } from '/lib/xp/content';
 import { OfficeContent } from '../../../../office-pages/types';
+import { forceArray } from '../../../../utils/array-utils';
+import { OfficeBranch } from '../../../../../site/content-types/office-branch/office-branch';
 
-export const buildOfficeIngress = (content: OfficeContent) => {};
+const DEFAULT_PHONE = '55 55 33 33';
+
+const buildAddressElement = (address: NonNullable<OfficeBranch['postadresse']>) => {
+    const {
+        gatenavn = '',
+        husnummer = '',
+        husbokstav = '',
+        postnummer = '',
+        poststed = '',
+    } = address;
+
+    const husNrOgBokstav = husnummer || husbokstav ? ` ${husnummer}${husbokstav}` : '';
+
+    return `<b>Publikumsmottak: </b>${gatenavn}${husNrOgBokstav}, ${postnummer} ${poststed.toUpperCase()}`;
+};
+
+const buildOfficeInfoLegacy = (content: Content<'no.nav.navno:office-information'>) => {
+    const phone = content.data.kontaktinformasjon?.telefonnummer || DEFAULT_PHONE;
+    const phoneElement = `<b>Telefon:</b> ${phone}`;
+
+    const address = forceArray(content.data.kontaktinformasjon?.publikumsmottak).find(
+        (mottak) => mottak?.besoeksadresse?.type === 'stedsadresse'
+    )?.besoeksadresse;
+
+    if (!address) {
+        return phoneElement;
+    }
+
+    const addressElement = buildAddressElement(address);
+
+    return `<b>${phone}</b><br/>`;
+};
+
+export const buildOfficeIngress = (content: OfficeContent): string => {
+    if (content.type === 'no.nav.navno:office-information') {
+        return buildOfficeInfoLegacy(content);
+    }
+
+    return '';
+};
