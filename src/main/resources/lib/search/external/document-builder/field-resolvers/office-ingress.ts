e338b24bd3e32441e4c46abe8e80f9717c5f36ee
Anders Bråthen Nomerstad <anders@nomerstad.com>
2024-02-09 17:20:37 +0100

    Spesialhåndtering for ingress til kontorsider i nytt søk

--- src/main/resources/lib/search/external/document-builder/field-resolvers/office-ingress.ts
+++ src/main/resources/lib/search/external/document-builder/field-resolvers/office-ingress.ts
@@ -1,11 +1,23 @@
-import { Content } from '/lib/xp/content';
 import { OfficeContent } from '../../../../office-pages/types';
 import { forceArray } from '../../../../utils/array-utils';
 import { OfficeBranch } from '../../../../../site/content-types/office-branch/office-branch';
 
+type Publikumsmottak = NonNullable<NonNullable<OfficeBranch['brukerkontakt']>['publikumsmottak']>;
+
 const DEFAULT_PHONE = '55 55 33 33';
 
-const buildAddressElement = (address: NonNullable<OfficeBranch['postadresse']>) => {
+const buildPhoneElement = (phoneNr?: string) =>
+    `<strong>Telefon:</strong> ${phoneNr || DEFAULT_PHONE}`;
+
+const buildAddressElement = (mottakList?: Publikumsmottak) => {
+    const address = forceArray(mottakList).find(
+        (mottak) => mottak.besoeksadresse?.type === 'stedsadresse'
+    )?.besoeksadresse;
+
+    if (!address) {
+        return null;
+    }
+
     const {
         gatenavn = '',
         husnummer = '',
@@ -16,30 +28,25 @@ const buildAddressElement = (address: NonNullable<OfficeBranch['postadresse']>) 
 
     const husNrOgBokstav = husnummer || husbokstav ? ` ${husnummer}${husbokstav}` : '';
 
-    return `<b>Publikumsmottak: </b>${gatenavn}${husNrOgBokstav}, ${postnummer} ${poststed.toUpperCase()}`;
-};
-
-const buildOfficeInfoLegacy = (content: Content<'no.nav.navno:office-information'>) => {
-    const phone = content.data.kontaktinformasjon?.telefonnummer || DEFAULT_PHONE;
-    const phoneElement = `<b>Telefon:</b> ${phone}`;
-
-    const address = forceArray(content.data.kontaktinformasjon?.publikumsmottak).find(
-        (mottak) => mottak?.besoeksadresse?.type === 'stedsadresse'
-    )?.besoeksadresse;
-
-    if (!address) {
-        return phoneElement;
-    }
-
-    const addressElement = buildAddressElement(address);
-
-    return `<b>${phone}</b><br/>`;
+    return `<strong>Publikumsmottak: </strong>${gatenavn}${husNrOgBokstav}, ${postnummer} ${poststed.toUpperCase()}`;
 };
 
 export const buildOfficeIngress = (content: OfficeContent): string => {
-    if (content.type === 'no.nav.navno:office-information') {
-        return buildOfficeInfoLegacy(content);
+    const isLegacyType = content.type === 'no.nav.navno:office-information';
+
+    const phoneElement = buildPhoneElement(
+        isLegacyType ? content.data.kontaktinformasjon?.telefonnummer : DEFAULT_PHONE
+    );
+
+    const addressElement = buildAddressElement(
+        isLegacyType
+            ? content.data.kontaktinformasjon?.publikumsmottak
+            : content.data.brukerkontakt?.publikumsmottak
+    );
+
+    if (!addressElement) {
+        return phoneElement;
     }
 
-    return '';
+    return `${phoneElement}<br/>${addressElement}`;
 };
