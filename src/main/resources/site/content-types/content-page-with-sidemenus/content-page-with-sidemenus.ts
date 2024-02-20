ec2db3d8aa3913e37913cbe4a9e698288657f6bb
Terje Karlsen <terjeofnorway@gmail.com>
2024-02-20 14:06:33 +0100

    Endrer til option-set for metadata av aktuelle samarbeidspartnere

--- src/main/resources/site/content-types/content-page-with-sidemenus/content-page-with-sidemenus.ts
+++ src/main/resources/site/content-types/content-page-with-sidemenus/content-page-with-sidemenus.ts
@@ -113,13 +113,91 @@ export interface ContentPageWithSidemenus {
      */
     provider: {
       /**
-       * Enkeltsamarbeid
+       * Velg samarbeidspartner
        */
       providerList: Array<{
         /**
-         * Velg underkategorier
+         * Type samarbeidspartner
          */
-        provider_audience: Array<"doctor" | "municipality_employed" | "optician" | "administrator" | "measures_organizer" | "aid_supplier" | "other">;
+        subProviders: Array<
+          | {
+              /**
+               * Selected
+               */
+              _selected: "doctor";
+
+              /**
+               * Lege, tannlege eller annen behandler
+               */
+              doctor: Record<string, unknown>;
+            }
+          | {
+              /**
+               * Selected
+               */
+              _selected: "municipality_employed";
+
+              /**
+               * Ansatt i kommunen eller fylkeskommunen
+               */
+              municipality_employed: Record<string, unknown>;
+            }
+          | {
+              /**
+               * Selected
+               */
+              _selected: "optician";
+
+              /**
+               * Optiker eller øyelege
+               */
+              optician: Record<string, unknown>;
+            }
+          | {
+              /**
+               * Selected
+               */
+              _selected: "administrator";
+
+              /**
+               * Bostyrer
+               */
+              administrator: Record<string, unknown>;
+            }
+          | {
+              /**
+               * Selected
+               */
+              _selected: "measures_organizer";
+
+              /**
+               * Tiltaksarrangør
+               */
+              measures_organizer: Record<string, unknown>;
+            }
+          | {
+              /**
+               * Selected
+               */
+              _selected: "aid_supplier";
+
+              /**
+               * Hjelpemiddelformidler
+               */
+              aid_supplier: Record<string, unknown>;
+            }
+          | {
+              /**
+               * Selected
+               */
+              _selected: "other";
+
+              /**
+               * Andre samarbeidspartnere
+               */
+              other: Record<string, unknown>;
+            }
+        >;
 
         /**
          * Innhold
