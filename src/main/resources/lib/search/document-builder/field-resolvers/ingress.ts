aed2d9a669ae73aa746a6358155284ccd8b4433f
Anders Nomerstad <anders@nomerstad.com>
2024-04-16 16:45:12 +0200

    Skriver om kontor ingress til søket

--- src/main/resources/lib/search/document-builder/field-resolvers/ingress.ts
+++ src/main/resources/lib/search/document-builder/field-resolvers/ingress.ts
@@ -1,78 +1,34 @@
 import { OfficeContent } from '../../../office-pages/types';
-import { forceArray, removeDuplicatesFilter } from '../../../utils/array-utils';
-import { OfficeBranch } from '@xp-types/site/content-types/office-branch';
-
-type Publikumsmottak = NonNullable<NonNullable<OfficeBranch['brukerkontakt']>['publikumsmottak']>;
-type WithBesoeksAdresse = NonNullable<Pick<Publikumsmottak[number], 'besoeksadresse'>>;
-
-const DEFAULT_PHONE = '55 55 33 33';
+import { forceArray } from '../../../utils/array-utils';
+import { capitalize } from '../../../utils/string-utils';
 
 const INGRESS_MAX_LENGTH = 500;
 
-const buildPhoneElement = (phoneNr?: string) =>
-    `<strong>Telefon:</strong> ${phoneNr || DEFAULT_PHONE}`;
-
-const getPublikumsmottak = (content: OfficeContent): Publikumsmottak | undefined => {
-    const { type, data } = content;
-
-    switch (type) {
-        case 'no.nav.navno:office-page':
-            return data.officeNorgData.data.brukerkontakt?.publikumsmottak;
-        case 'no.nav.navno:office-branch':
-            return data.brukerkontakt?.publikumsmottak;
-        case 'no.nav.navno:office-information':
-            return data.kontaktinformasjon?.publikumsmottak;
-    }
-};
-
-const buildAddressElement = (content: OfficeContent) => {
-    const publikumsmottak = getPublikumsmottak(content);
-
-    const validMottak = forceArray(publikumsmottak)
-        .filter(
-            (mottak): mottak is WithBesoeksAdresse => mottak.besoeksadresse?.type === 'stedsadresse'
-        )
-        .filter(
-            removeDuplicatesFilter(
-                (a, b) => a.besoeksadresse?.postnummer === b.besoeksadresse?.postnummer
-            )
-        );
-
-    if (validMottak.length === 0) {
-        return null;
-    }
-
-    const addresses = validMottak.map((mottak) => {
-        const {
-            gatenavn = '',
-            husnummer = '',
-            husbokstav = '',
-            postnummer = '',
-            poststed = '',
-        } = mottak.besoeksadresse as any;
-
-        const husNrOgBokstav = husnummer || husbokstav ? ` ${husnummer}${husbokstav}` : '';
-        return `${gatenavn}${husNrOgBokstav}, ${postnummer} ${poststed.toUpperCase()}`;
-    });
-
-    const addressesList =
-        addresses.length === 1
-            ? addresses[0]
-            : `<ul>${addresses.map((address) => `<li>${address}</li>`).join('')}</ul>`;
-
-    return `<strong>Publikumsmottak:</strong> ${addressesList}`;
-};
+const DEFAULT_OFFICE_INGRESS = 'Kontorinformasjon';
 
 export const buildSearchDocumentOfficeIngress = (content: OfficeContent) => {
-    const phoneElement = buildPhoneElement(
-        content.type === 'no.nav.navno:office-information'
-            ? content.data.kontaktinformasjon?.telefonnummer
-            : DEFAULT_PHONE
-    );
+    const isLocalOffice = content.type === 'no.nav.navno:office-branch';
+    if (!isLocalOffice) {
+        return DEFAULT_OFFICE_INGRESS;
+    }
 
-    const addressElement = buildAddressElement(content);
+    const poststeder = forceArray(content.data.brukerkontakt?.publikumsmottak)
+        .filter(
+            (mottak) =>
+                mottak.besoeksadresse?.type === 'stedsadresse' && !!mottak.besoeksadresse.poststed
+        )
+        .map((mottak) => capitalize(mottak.besoeksadresse?.poststed as string));
 
-    return addressElement ? `${phoneElement}<br/>${addressElement}` : phoneElement;
+    if (poststeder.length === 0) {
+        return DEFAULT_OFFICE_INGRESS;
+    }
+
+    const poststederStr =
+        poststeder.length === 1
+            ? poststeder[0]
+            : `${poststeder.slice(0, -1).join(', ')} og ${poststeder.slice(-1)}`;
+
+    return `Lokalkontor for ${poststederStr}`;
 };
 
 const withoutTable = (text: string) => text.split('<table')[0];
--- src/main/resources/lib/utils/string-utils.ts
+++ src/main/resources/lib/utils/string-utils.ts
@@ -1,1 +1,11 @@
 export const stripLineBreaks = (str: string) => str.replace(/\r?\n|\r/g, '');
+
+export const capitalize = (str: string) =>
+    str
+        .split(' ')
+        .map((letter) => {
+            return `${letter
+                .toLowerCase()
+                .replace(/(^|[\s-])\S/g, (letter) => letter.toUpperCase())}`;
+        })
+        .join(' ');
