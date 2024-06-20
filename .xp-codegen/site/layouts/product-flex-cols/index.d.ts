4f64ffd4e6df7d587796d7f4e1d3082da65e7cac
Terje Karlsen <terjeofnorway@gmail.com>
2024-06-20 10:13:03 +0200

    Fjerner sjekkboks for å vise 'kopier lenke'-knapp

--- .xp-codegen/site/layouts/product-flex-cols/index.d.ts
+++ .xp-codegen/site/layouts/product-flex-cols/index.d.ts
@@ -14,9 +14,4 @@ export type ProductFlexCols = {
    * Ikke vis under innhold
    */
   hideFromInternalNavigation: boolean;
-
-  /**
-   * Toggle "kopier lenke" knapp
-   */
-  toggleCopyButton: boolean;
 }
