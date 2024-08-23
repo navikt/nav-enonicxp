5a59ae7937a98fb0ca0068da2d7954d141198222
Terje Karlsen <terjeofnorway@gmail.com>
2024-08-23 12:34:36 +0200

    Fjerner logging og setter legacy import til 10. minutt

--- src/main/resources/lib/office-pages/_legacy-office-information/legacy-office-update.ts
+++ src/main/resources/lib/office-pages/_legacy-office-information/legacy-office-update.ts
@@ -93,11 +93,10 @@ const updateOfficeInfo = (officeInformationUpdated: OfficeInformation[]) => {
         officesInNorg[enhet.enhetId] = true;
 
         const updatedName = commonLib.sanitize(enhet.navn);
+
         // Temporary check to only import lagging HMS that hasn't been
         // transferred to new layout
-
         if (enhet.type === 'HMS' && !remainingHMSToImport.has(updatedName)) {
-            log.info(`Skipping hmsimport ${updatedName}`);
             return;
         }
 
@@ -302,7 +301,7 @@ export const startOfficeInfoPeriodicUpdateSchedule = () => {
         jobDescription: 'Updates legacy office information from norg2 every hour',
         jobSchedule: {
             type: 'CRON',
-            value: '*/5 * * * *',
+            value: '*/10 * * * *',
             timeZone: 'GMT+2:00',
         },
         taskDescriptor: officeInfoUpdateTaskDescriptor,
