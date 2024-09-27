5c8090ee4d7bf0e05e831aaeb2e4e93e3b551224
Anders Nomerstad <anders@nomerstad.com>
2024-09-27 16:52:24 +0200

    En faktisk test \o/

--- e2e-tests/tests.test.ts
+++ e2e-tests/tests.test.ts
@@ -1,3 +1,23 @@
-test('What', () => {
-    expect(true).toBe(true);
+import { GenericContainer, StartedTestContainer, Wait } from 'testcontainers';
+
+describe('Tester testcontainers', () => {
+    let container: StartedTestContainer;
+
+    beforeAll(async () => {
+        const image = await GenericContainer.fromDockerfile('./xp-image').build();
+        container = await image
+            .withExposedPorts(8080)
+            .withWaitStrategy(Wait.forLogMessage(/.*Finished running main.*/, 1))
+            .start();
+    }, 20000);
+
+    test('App responds to liveness check', async () => {
+        const port = container.getMappedPort(8080);
+
+        const isAliveResponse = await fetch(
+            `http://localhost:${port}/_/service/no.nav.navno/isAlive`
+        );
+
+        expect(isAliveResponse.status).toBe(200);
+    }, 20000);
 });
