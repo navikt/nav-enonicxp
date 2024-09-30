0e908472ae1dfe26f71601b2553c85f1f143e4d6
Anders Nomerstad <anders@nomerstad.com>
2024-09-30 19:36:00 +0200

    Mulighet for å gjenbruke XP container

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -126,7 +126,7 @@ module.exports = {
             },
         },
         {
-            'files': ['*.test.ts', 'src/main/resources/__test/**/*.*'],
+            'files': ['*.test.ts', 'src/main/resources/__test/**/*.*', 'e2e-tests/**/*.*'],
             'rules': {
                 'no-console': 'off',
                 'no-restricted-imports': 'off',
