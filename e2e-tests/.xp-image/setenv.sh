ce6afab2ae752a0c6bbef67e4cac3f3f54ff0cb1
Anders Nomerstad <anders@nomerstad.com>
2024-10-10 19:09:17 +0200

    Fikser test-data-app build

--- e2e-tests/.xp-image/setenv.sh
+++ e2e-tests/.xp-image/setenv.sh
@@ -6,4 +6,3 @@ set -e
 echo "Install bootstrap apps"
 app.sh add https://repo.enonic.com/public/com/enonic/app/contentstudio/5.2.2/contentstudio-5.2.2.jar
 app.sh add file:///enonic-xp/home/navno.jar --force
-app.sh add file:///enonic-xp/home/navno-testdata.jar --force
