35c71a20ca83521ba277d26d42684fb406986d4f
Anders Nomerstad <anders@nomerstad.com>
2024-10-11 16:57:18 +0200

    Gjeninnfører babel ignore

--- .babelrc.js
+++ .babelrc.js
@@ -1,4 +1,5 @@
 export default {
     plugins: ['@anders-nom/babel-plugin-import-graphql-string', './babel-logger-transformer'],
     presets: ['@babel/preset-env', '@babel/preset-typescript'],
+    ignore: ['./test/**/*.*'],
 };
