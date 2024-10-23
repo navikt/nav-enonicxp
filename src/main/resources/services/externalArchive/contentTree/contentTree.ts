9748d00d01080be417015399ff0d9a5230efd428
Anders Nomerstad <anders@nomerstad.com>
2024-10-23 13:56:35 +0200

    Cacher icon-data

--- src/main/resources/services/externalArchive/contentTree/contentTree.ts
+++ src/main/resources/services/externalArchive/contentTree/contentTree.ts
@@ -5,6 +5,7 @@ import { getNavnoContentPath, stripPathPrefix } from '../../../lib/paths/path-ut
 import { validateServiceSecretHeader } from '../../../lib/utils/auth-utils';
 import { ContentDescriptor } from '../../../types/content-types/content-config';
 import { logger } from '../../../lib/utils/logging';
+import cacheLib from '/lib/cache';
 
 type ContentTreeEntry = {
     id: string;
@@ -13,8 +14,6 @@ type ContentTreeEntry = {
     displayName: string;
     type: ContentDescriptor;
     numChildren: number;
-    withChildren: boolean;
-    children?: ContentTreeEntry[];
     icon?: {
         data: string;
         mimeType: string;
@@ -23,28 +22,31 @@ type ContentTreeEntry = {
 
 const MAX_CHILDREN = 1000;
 
-const getIcon = (type: ContentDescriptor) => {
-    const icon = contentLib.getType(type)?.icon;
+const iconsCache = cacheLib.newCache({ size: 100, expire: 3600 * 24 });
 
-    if (!icon) {
-        return undefined;
-    }
+const getIcon = (type: ContentDescriptor) =>
+    iconsCache.get(type, () => {
+        const icon = contentLib.getType(type)?.icon;
 
-    try {
-        return {
-            data: ioLib.readText(icon.data),
-            mimeType: icon.mimeType,
-        };
-    } catch (e) {
-        logger.error(`Failed to read icon data stream - ${e}`);
-        return undefined;
-    }
-};
+        if (!icon) {
+            return undefined;
+        }
 
-const transformToContentTreeEntry = (content: Content, withChildren: boolean): ContentTreeEntry => {
+        try {
+            return {
+                data: ioLib.readText(icon.data),
+                mimeType: icon.mimeType,
+            };
+        } catch (e) {
+            logger.error(`Failed to read icon data stream - ${e}`);
+            return undefined;
+        }
+    });
+
+const transformToContentTreeEntry = (content: Content): ContentTreeEntry => {
     const childrenResult = contentLib.getChildren({
         key: content._id,
-        count: withChildren ? MAX_CHILDREN : 0,
+        count: 0,
     });
 
     if (childrenResult.total > MAX_CHILDREN) {
@@ -53,12 +55,6 @@ const transformToContentTreeEntry = (content: Content, withChildren: boolean): C
         );
     }
 
-    const children = withChildren
-        ? childrenResult.hits.map((childContent) =>
-              transformToContentTreeEntry(childContent, false)
-          )
-        : undefined;
-
     return {
         id: content._id,
         path: stripPathPrefix(content._path),
@@ -66,15 +62,13 @@ const transformToContentTreeEntry = (content: Content, withChildren: boolean): C
         displayName: content.displayName,
         type: content.type,
         numChildren: childrenResult.total,
-        withChildren,
-        children,
         icon: getIcon(content.type),
     };
 };
 
 const getContentTreeChildren = (path: string) => {
     const children = contentLib.getChildren({ key: getNavnoContentPath(path), count: 1000 }).hits;
-    return children.map((content) => transformToContentTreeEntry(content, true));
+    return children.map(transformToContentTreeEntry);
 };
 
 type Params = Partial<{
@@ -119,7 +113,7 @@ export const externalArchiveContentTreeGet = (req: XP.Request) => {
     return {
         status: 200,
         body: {
-            current: transformToContentTreeEntry(parentContent, false),
+            current: transformToContentTreeEntry(parentContent),
             children: getContentTreeChildren(path),
         },
         contentType: 'application/json',
