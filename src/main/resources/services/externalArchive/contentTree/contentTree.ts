14c89c82fa4e0c80c8cbda9b4a32bbf08ff291d3
Anders Nomerstad <anders@nomerstad.com>
2024-10-29 08:59:56 +0100

    Støtter layers i contentTree service

--- src/main/resources/services/externalArchive/contentTree/contentTree.ts
+++ src/main/resources/services/externalArchive/contentTree/contentTree.ts
@@ -1,8 +1,12 @@
-import * as contentLib from '/lib/xp/content';
 import { Content } from '/lib/xp/content';
 import { getNavnoContentPath, stripPathPrefix } from '../../../lib/paths/path-utils';
 import { validateServiceSecretHeader } from '../../../lib/utils/auth-utils';
 import { ContentDescriptor } from '../../../types/content-types/content-config';
+import { getLayersData, isValidLocale } from '../../../lib/localization/layers-data';
+import { getRepoConnection } from '../../../lib/utils/repo-utils';
+import { RepoConnection } from '/lib/xp/node';
+import { forceArray } from '../../../lib/utils/array-utils';
+import { logger } from '../../../lib/utils/logging';
 
 type ContentTreeEntry = {
     id: string;
@@ -13,10 +17,13 @@ type ContentTreeEntry = {
     numChildren: number;
 };
 
-const transformToContentTreeEntry = (content: Content): ContentTreeEntry => {
-    const childrenResult = contentLib.getChildren({
-        key: content._id,
-        count: 0,
+// TODO: implement pagination with smaller chunks
+const MAX_CHILDREN_COUNT = 1000;
+
+const transformToContentTreeEntry = (content: Content, repo: RepoConnection): ContentTreeEntry => {
+    const childrenResult = repo.findChildren({
+        parentKey: content._id,
+        countOnly: true,
     });
 
     return {
@@ -29,13 +36,28 @@ const transformToContentTreeEntry = (content: Content): ContentTreeEntry => {
     };
 };
 
-const getContentTreeChildren = (path: string) => {
-    const children = contentLib.getChildren({ key: getNavnoContentPath(path), count: 1000 }).hits;
-    return children.map(transformToContentTreeEntry);
+const getContentTreeChildren = (path: string, repo: RepoConnection): ContentTreeEntry[] => {
+    const findChildrenResult = repo.findChildren({
+        parentKey: getNavnoContentPath(path),
+        count: MAX_CHILDREN_COUNT,
+    });
+
+    if (findChildrenResult.total > MAX_CHILDREN_COUNT) {
+        logger.error(
+            `Found ${findChildrenResult.total} children count exceeds the maximum ${MAX_CHILDREN_COUNT}!`
+        );
+    }
+
+    const ids = findChildrenResult.hits.map((hit) => hit.id);
+
+    const childContents = repo.get<Content>(ids);
+
+    return forceArray(childContents).map((content) => transformToContentTreeEntry(content, repo));
 };
 
 type Params = Partial<{
     path: string;
+    locale: string;
 }>;
 
 export const externalArchiveContentTreeGet = (req: XP.Request) => {
@@ -49,7 +71,7 @@ export const externalArchiveContentTreeGet = (req: XP.Request) => {
         };
     }
 
-    const { path } = req.params as Params;
+    const { path, locale } = req.params as Params;
 
     if (!path) {
         return {
@@ -61,7 +83,23 @@ export const externalArchiveContentTreeGet = (req: XP.Request) => {
         };
     }
 
-    const parentContent = contentLib.get({ key: getNavnoContentPath(path) });
+    if (!isValidLocale(locale)) {
+        return {
+            status: 400,
+            body: {
+                message: `Locale ${locale} is not valid`,
+            },
+            contentType: 'application/json',
+        };
+    }
+
+    const repo = getRepoConnection({
+        repoId: getLayersData().localeToRepoIdMap[locale],
+        branch: 'draft',
+        asAdmin: true,
+    });
+
+    const parentContent = repo.get<Content>({ key: getNavnoContentPath(path) });
 
     if (!parentContent) {
         return {
@@ -76,8 +114,8 @@ export const externalArchiveContentTreeGet = (req: XP.Request) => {
     return {
         status: 200,
         body: {
-            current: transformToContentTreeEntry(parentContent),
-            children: getContentTreeChildren(path),
+            current: transformToContentTreeEntry(parentContent, repo),
+            children: getContentTreeChildren(path, repo),
         },
         contentType: 'application/json',
     };
