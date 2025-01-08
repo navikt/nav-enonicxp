b67d6d4ff402155a37e30e1891bc89b47214df25
Terje Karlsen <terjeofnorway@gmail.com>
2025-01-08 16:23:02 +0100

    Sørger for at referanser ikke resolves

--- src/main/resources/lib/contentlists/remove-unpublished.ts
+++ src/main/resources/lib/contentlists/remove-unpublished.ts
@@ -88,14 +88,13 @@ const removeUnpublishedFromContentList = (
             });
 
             if (shouldPushChanges) {
-                /*
                 repoConnection.commit({ keys: contentList._id });
                 repoConnection.push({
                     key: contentList._id,
                     target: 'master',
                     includeChildren: false,
+                    resolve: false,
                 });
-                */
             } else {
                 log.info(
                     `Removed unpublished content from content list ${contentList._id}, but draft and master are out of sync, so not pushing changes.`
