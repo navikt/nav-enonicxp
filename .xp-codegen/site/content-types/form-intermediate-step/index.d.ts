f6016e1fab080523ef6b4b34aa8166b5871fc352
Terje Karlsen <terjeofnorway@gmail.com>
2025-03-18 13:54:47 +0100

    Legger til ekstra steg for mellomsteg

--- .xp-codegen/site/content-types/form-intermediate-step/index.d.ts
+++ .xp-codegen/site/content-types/form-intermediate-step/index.d.ts
@@ -203,6 +203,84 @@ export type FormIntermediateStep = {
                        */
                       externalUrl: string;
                     };
+                  }
+                | {
+                    /**
+                     * Selected
+                     */
+                    _selected: "next";
+
+                    /**
+                     * Nytt steg
+                     */
+                    next: {
+                      /**
+                       * Redaksjonelt innhold ovenfor valgene
+                       */
+                      editorial?: string;
+
+                      /**
+                       * Overskrift til stegene
+                       */
+                      stepsHeadline?: string;
+
+                      /**
+                       * Steg
+                       */
+                      steps: Array<{
+                        /**
+                         * Tittel på valget
+                         */
+                        label: string;
+
+                        /**
+                         * Ekstra forklaring
+                         */
+                        explanation?: string;
+
+                        /**
+                         * Informasjon om språk
+                         */
+                        languageDisclaimer?: string;
+
+                        /**
+                         * Dette valget sender brukeren videre til:
+                         */
+                        nextStep:
+                          | {
+                              /**
+                               * Selected
+                               */
+                              _selected: "internal";
+
+                              /**
+                               * Intern lenke til skjema, søknad eller klage
+                               */
+                              internal: {
+                                /**
+                                 * Internt innhold
+                                 */
+                                internalContent: string;
+                              };
+                            }
+                          | {
+                              /**
+                               * Selected
+                               */
+                              _selected: "external";
+
+                              /**
+                               * Ekstern lenke til skjema, søknad eller klage
+                               */
+                              external: {
+                                /**
+                                 * Ekstern URL
+                                 */
+                                externalUrl: string;
+                              };
+                            };
+                      }>;
+                    };
                   };
             }>;
           };
