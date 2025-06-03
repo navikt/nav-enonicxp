638dad48339444ac1449448c679c16f9f7b6ae11
Terje Karlsen <terjeofnorway@gmail.com>
2025-06-03 15:19:16 +0200

    Forsøker widget med iframe for visning av arkivinnhold

--- src/main/resources/admin/widgets/archive-preview/archive-preview.ts
+++ src/main/resources/admin/widgets/archive-preview/archive-preview.ts
@@ -1,24 +1,11 @@
-import httpClient, { HttpResponse } from '/lib/http-client';
-
 import thymeleafLib from '/lib/thymeleaf';
 
 const view = resolve('./archive-preview.html');
 
 export const get = (req: XP.Request) => {
-    const model = { invalidate: 'https://www.nav.no' };
+    const url = `${req.scheme}://${req.host}:${req.port}/admin/site/inline/default/draft${req.params.contentPath}`;
 
-    log.info(`Preview site engine request: ${JSON.stringify(req)}`);
-
-    const response = httpClient.request({
-        url: 'http://localhost:8080/admin/site/inline/navno-engelsk/draft/barnetrygd-copy-2',
-        method: 'GET',
-        contentType: 'application/json',
-        headers: {
-            secret: app.config.serviceSecret,
-        },
-    });
-
-    log.info(`Fetch result: ${JSON.stringify(response)}`);
+    const model = { archivePreviewUrl: url };
 
     return {
         headers: {
