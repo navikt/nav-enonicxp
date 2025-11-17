4faa62f3ab73f7b5453021ce248b841aa51986f8
Terje Karlsen <terje@terjeofnorway.no>
2025-11-17 13:18:38 +0100

    Legger til callback for 'lastPublish'

---
+++ test/unit-tests/tests/lib/guillotine/schema/schema-creation-callbacks/generaldata.test.ts
@@ -0,0 +1,26 @@
+import * as contentLib from '/lib/xp/content';
+import { determineLastPublished } from '@navno-app/lib/guillotine/schema/schema-creation-callbacks/general-data-callback';
+
+const partialContent = {
+    type: 'no.nav.navno:dynamic-page',
+    publish: {
+        from: '2025-06-01T10:00:00.000Z',
+        first: '2025-05-25T13:33:00.000Z',
+    },
+    _ts: '2025-05-25T13:33:00.000Z',
+} as any;
+
+describe('TestotdetermineLastPublished', () => {
+    test('Should use publish.from when this is the latest date.', () => {
+        partialContent.publish.from = '2025-06-01T10:00:00.000Z';
+        const lastPublished = determineLastPublished(partialContent as any);
+
+        expect(lastPublished).toBe('2025-06-01T10:00:00.000Z');
+    });
+    test('Should use _ts when this is the latest date.', () => {
+        partialContent._ts = '2025-07-01T12:00:00.000Z';
+        const lastPublished = determineLastPublished(partialContent as any);
+
+        expect(lastPublished).toBe('2025-07-01T12:00:00.000Z');
+    });
+});
