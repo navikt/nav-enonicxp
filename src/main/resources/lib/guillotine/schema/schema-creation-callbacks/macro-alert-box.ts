fec923bc0b2024ab8c822f3c716e0bf964a96fb4
perolavm <per.olav.mariussen@nav.no>
2025-11-20 13:41:27 +0100

    Revert bumb html-enteties

--- src/main/resources/lib/guillotine/schema/schema-creation-callbacks/macro-alert-box.ts
+++ src/main/resources/lib/guillotine/schema/schema-creation-callbacks/macro-alert-box.ts
@@ -1,6 +1,6 @@
 import macroLib from '/lib/guillotine/macro';
 import { CreationCallback } from '../../utils/creation-callback-utils';
-import { decode } from '/assets/html-entities/2.6.0/lib';
+import { decode } from '/assets/html-entities/2.5.2/lib';
 import striptags from '/assets/striptags/3.2.0/src/striptags';
 
 export const macroAlertboxCallback: CreationCallback = (context, params) => {
