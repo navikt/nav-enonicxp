18e99f0c622ac5023dfecb1c48d74dcd4c2da616
perolavm <per.olav.mariussen@nav.no>
2025-12-18 13:13:33 +0100

    Bump actions github/nais

--- .xp-codegen/site/content-types/dynamic-page/index.d.ts
+++ .xp-codegen/site/content-types/dynamic-page/index.d.ts
@@ -36,12 +36,12 @@ export type DynamicPage = {
   canonicalUrl?: string;
 
   /**
-   * Ikke vis "tilfeldige "
+   * Skal ikke vises i søk
    */
   noindex: boolean;
 
   /**
-   * Ikke vis "tilfeldige" utdrag (snippets) i Google-søk
+   * Ikke vis "tilfeldige" utdrag (snippets). NB! Skal bare brukes av hovedredaktør/administrator.
    */
   nosnippet: boolean;
 
--- .xp-codegen/site/content-types/section-page/index.d.ts
+++ .xp-codegen/site/content-types/section-page/index.d.ts
@@ -111,12 +111,12 @@ export type SectionPage = {
   canonicalUrl?: string;
 
   /**
-   * Ikke vis "tilfeldige "
+   * Skal ikke vises i søk
    */
   noindex: boolean;
 
   /**
-   * Ikke vis "tilfeldige" utdrag (snippets) i Google-søk
+   * Ikke vis "tilfeldige" utdrag (snippets). NB! Skal bare brukes av hovedredaktør/administrator.
    */
   nosnippet: boolean;
 
