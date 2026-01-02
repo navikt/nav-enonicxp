7f0a0732db8b9348d6fa4ef616f1b50c80a9d4f4
Terje Karlsen <terje@terjeofnorway.no>
2026-01-02 10:11:44 +0100

    Forbedrer tester slik at de ikke er avhengig av hverandre

--- test/unit-tests/tests/lib/guillotine/schema/schema-creation-callbacks/generaldata.test.ts
+++ test/unit-tests/tests/lib/guillotine/schema/schema-creation-callbacks/generaldata.test.ts
@@ -1,25 +1,27 @@
-import * as contentLib from '/lib/xp/content';
 import { determineLastPublished } from '@navno-app/lib/guillotine/schema/schema-creation-callbacks/general-data-callback';
 
-const partialContent = {
-    type: 'no.nav.navno:dynamic-page',
-    publish: {
-        from: '2025-06-01T10:00:00.000Z',
-        first: '2025-05-25T13:33:00.000Z',
-    },
-    _ts: '2025-05-25T13:33:00.000Z',
-} as any;
+const createPartialContent = () =>
+    ({
+        type: 'no.nav.navno:dynamic-page',
+        publish: {
+            from: '2025-06-01T10:00:00.000Z',
+            first: '2025-05-25T13:33:00.000Z',
+        },
+        _ts: '2025-05-25T13:33:00.000Z',
+    }) as any;
 
 describe('Test of determineLastPublished', () => {
     test('Should use publish.from when this is the latest date.', () => {
+        const partialContent = createPartialContent();
         partialContent.publish.from = '2025-06-01T10:00:00.000Z';
-        const lastPublished = determineLastPublished(partialContent as any);
+        const lastPublished = determineLastPublished(partialContent);
 
         expect(lastPublished).toBe('2025-06-01T10:00:00.000Z');
     });
     test('Should use _ts when this is the latest date.', () => {
+        const partialContent = createPartialContent();
         partialContent._ts = '2025-07-01T12:00:00.000Z';
-        const lastPublished = determineLastPublished(partialContent as any);
+        const lastPublished = determineLastPublished(partialContent);
 
         expect(lastPublished).toBe('2025-07-01T12:00:00.000Z');
     });
